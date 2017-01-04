9f9a39df6df565049a8a28493471d44c79b731be
Mihailo Isakov <mihailo@protonmail.com>
2017-01-04 11:38:15 -0500

    valid signal is a vector now

--- layer_multiplex_nn/tb_input_aggregator2.v
+++ layer_multiplex_nn/tb_input_aggregator2.v
@@ -30,16 +30,19 @@ module tb_input_aggregator2;
 	reg start;
 	reg [53:0] start_input;
 	reg [53:0] layer_input;
-	reg layer_input_valid;
+	reg [5:0] layer_input_valid;
 
 	// Outputs
-	wire [1:0] layer_num;
+    wire [5:0] active;
+	wire [2:0] layer_num;
 	wire [53:0] out_inputs;
 	wire [611:0] out_weights;
 	wire layer_start;
 
 	// Instantiate the Unit Under Test (UUT)
-	input_aggregator uut (
+	input_aggregator 
+    #(.LAYER_SIZES({6'b101010, 6'b111010, 6'b111110, 6'b111111})) 
+    uut (
 		.clk(clk), 
 		.rst(rst), 
 		.start(start), 
@@ -49,7 +52,8 @@ module tb_input_aggregator2;
 		.layer_num(layer_num), 
 		.out_inputs(out_inputs), 
 		.out_weights(out_weights), 
-		.layer_start(layer_start)
+		.layer_start(layer_start),
+        .active(active)
 	);
 
     always
@@ -70,19 +74,22 @@ module tb_input_aggregator2;
         #20 start = 1;
         #2  start = 0;
 
-        #20 layer_input_valid = 1; // finshed layer 1
+        #20 layer_input_valid = 6'b111111; // finshed layer 1
         #2 layer_input_valid = 0;
 
-        #30 layer_input_valid = 1; // finshed layer 2
+        #30 layer_input_valid = 6'b111110; // finshed layer 2
         #2 layer_input_valid = 0;
 
-        #10 layer_input_valid = 1; // finshed layer 3
+        #10 layer_input_valid = 6'b111000; // not finished yet
         #2 layer_input_valid = 0;
 
-        #20 layer_input_valid = 1; // finshed layer 4 - last layer
+        #10 layer_input_valid = 6'b111010; // finshed layer 3
         #2 layer_input_valid = 0;
 
-        #20 layer_input_valid = 1; // testing if once in IDLE, the state should not change
+        #20 layer_input_valid = 6'b101010; // finshed layer 4 - last layer
+        #2 layer_input_valid = 0;
+
+        #20 layer_input_valid = 6'b001010; // testing if once in IDLE, the state should not change
         #2 layer_input_valid = 0;  // has to receive a new start from outside
 
 	end
