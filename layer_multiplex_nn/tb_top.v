5af618da93c87b871fecd3c203ca688eb269a459
Mihailo Isakov <mihailo@protonmail.com>
2017-01-04 11:41:00 -0500

    top module

---
+++ layer_multiplex_nn/tb_top.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   02:13:48 01/04/2017
+// Design Name:   top
+// Module Name:   /home/mihailo/development/projects/layer_multiplex_nn/layer_multiplex_nn/tb_top.v
+// Project Name:  layer_multiplex_nn
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: top
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tb_top;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg start;
+	reg [53:0] start_input;
+
+	// Outputs
+	wire [53:0] final_output;
+	wire [5:0] final_output_valid;
+
+	// Instantiate the Unit Under Test (UUT)
+	top uut (
+		.clk(clk), 
+		.rst(rst), 
+		.start(start), 
+		.start_input(start_input), 
+		.final_output(final_output), 
+		.final_output_valid(final_output_valid)
+	);
+
+    always
+        #1 clk = ~clk;
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		start = 0;
+		start_input = 0;
+
+        #20 rst = 1;
+        #20 rst = 0;
+
+        #20 start = 1;
+            start_input = 125904376;
+        #2  start = 0;
+
+        //#100 start = 1;
+            //start_input = 57602345;
+        //#2  start = 0;
+
+	end
+      
+endmodule
+
---
+++ layer_multiplex_nn/top.v
@@ -0,0 +1,78 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:02:18 01/03/2017 
+// Design Name: 
+// Module Name:    top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module top
+#(
+    parameter LAYER_MAX = 4,
+              NUM_NEURON = 6,      // max number of neurons
+              INPUT_SIZE = 9,      // width of the input signals
+              WEIGHT_SIZE = 17,    // width of the weight signals
+              ADDR_SIZE = 10,
+              WEIGHTS_INIT = "weights612.list",
+    parameter [NUM_NEURON*LAYER_MAX-1:0] LAYER_SIZES = {6'b101010, 6'b111010, 6'b111110, 6'b111111}
+)
+(
+    input clk,
+    input rst,
+    input start,
+    input [NUM_NEURON*INPUT_SIZE-1:0]  start_input,     // outside input received at the start
+    output [NUM_NEURON*INPUT_SIZE-1:0] final_output,
+    output [NUM_NEURON-1:0]            final_output_valid
+);
+
+   
+    wire [NUM_NEURON-1:0]                        active;
+    wire [NUM_NEURON*INPUT_SIZE-1:0]             layer_input;
+    wire [NUM_NEURON*NUM_NEURON*WEIGHT_SIZE-1:0] layer_weights;
+    wire [NUM_NEURON*ADDR_SIZE-1:0]              layer_output;
+    wire [NUM_NEURON-1:0]                        layer_output_valid;
+    wire                                         layer_start;
+
+    layer_controller #(
+        .NUM_NEURON(NUM_NEURON), .INPUT_SIZE(INPUT_SIZE), .WEIGHT_SIZE(WEIGHT_SIZE), .OUTPUT_SIZE(ADDR_SIZE), 
+        .LAYER_MAX(LAYER_MAX), .ADDR_SIZE(ADDR_SIZE), .WEIGHTS_INIT(WEIGHTS_INIT)
+    )
+    layer_controller (
+        .clk(clk),
+        .rst(rst),
+        .start(start),
+        .start_input(start_input),
+        .layer_output(layer_output),
+        .layer_output_valid(layer_output_valid),
+        .layer_start(layer_start),
+        .active(active),
+        .layer_input(layer_input),
+        .layer_weights(layer_weights)
+    );
+
+    layer #(.NUM_NEURON(NUM_NEURON), .NUM_INPUTS(NUM_NEURON), .INPUT_SIZE(INPUT_SIZE), .WEIGHT_SIZE(WEIGHT_SIZE), .OUTPUT_SIZE(ADDR_SIZE))
+    layer (
+        .clk(clk),
+        .rst(rst),
+        .start(layer_start),
+        .active(active),
+        .inputs(layer_input),
+        .weights(layer_weights),
+        .out_values(layer_output),
+        .out_valid(layer_output_valid)
+    );
+    
+
+endmodule
