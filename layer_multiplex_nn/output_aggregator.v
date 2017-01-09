2d26db535df74df2f2ead1d7efdc4b0508eb1131
Mihailo Isakov <mihailo@protonmail.com>
2017-01-09 14:12:14 -0500

    refactoring and docs

--- layer_multiplex_nn/output_aggregator.v
+++ layer_multiplex_nn/output_aggregator.v
@@ -7,19 +7,30 @@ module output_aggregator
 (
     input clk,
     input rst,
-    input [NUM_NEURON*ADDR_SIZE-1:0]   inputs_values, 
-    input [NUM_NEURON-1:0]             inputs_valid,
-    output [NUM_NEURON*VALUE_SIZE-1:0] outputs_values,
-    output [NUM_NEURON-1:0]            outputs_valid
+    input [NUM_NEURON*ADDR_SIZE-1:0]   inputs_values,  // inputs from the layer's neurons
+    input [NUM_NEURON-1:0]             inputs_valid,   // validity of the neuron's outputs
+    output [NUM_NEURON*VALUE_SIZE-1:0] outputs_values, // values passed through the sigmoid activation
+    output [NUM_NEURON-1:0]            outputs_valid   // their validity
 );
 
     reg [NUM_NEURON*VALUE_SIZE-1:0] outputs_buffer;
     reg [NUM_NEURON-1:0]            valid_buffer;  
 
-    wire lut_stable;
+    wire                             lut_stable;
     wire [NUM_NEURON*VALUE_SIZE-1:0] lut_outputs;
-    activation #(.NUM_NEURON(NUM_NEURON), .LUT_ADDR_SIZE(ADDR_SIZE), .LUT_DEPTH(1<<ADDR_SIZE), .LUT_WIDTH(VALUE_SIZE))
-        activation (.clk(clk), .rst(rst), .inputs(inputs_values), .outputs(lut_outputs), .stable(lut_stable));
+    activation #(
+        .NUM_NEURON(NUM_NEURON), 
+        .LUT_ADDR_SIZE(ADDR_SIZE), 
+        .LUT_DEPTH(1<<ADDR_SIZE), 
+        .LUT_WIDTH(VALUE_SIZE)
+    )
+    activation (
+        .clk(clk), 
+        .rst(rst), 
+        .inputs(inputs_values), 
+        .outputs(lut_outputs), 
+        .stable(lut_stable)
+    );
 
     always @ (posedge clk) begin
         if (rst) begin
