cdd91eb335c143e079b2a30b06e57dc4546ff7c4
Mihailo Isakov <mihailo@protonmail.com>
2017-01-25 23:23:31 -0500

    vector subtract updated

--- backpropagation/tb_vector_subtract.v
+++ backpropagation/tb_vector_subtract.v
@@ -4,15 +4,15 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   12:41:43 01/19/2017
-// Design Name:   vector_subtract
-// Module Name:   /home/mihailo/development/projects/layer_multiplex_nn/backpropagation/tb_vector_subtract.v
+// Create Date:   10:33:14 01/17/2017
+// Design Name:   vector_add
+// Module Name:   /home/mihailo/development/projects/layer_multiplex_nn/backpropagation/tb_vector_add.v
 // Project Name:  backpropagation
 // Target Device:  
 // Tool versions:  
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: vector_subtract
+// Verilog Test Fixture created by ISE for module: vector_add
 //
 // Dependencies:
 // 
@@ -23,29 +23,40 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 module tb_vector_subtract;
+    parameter VECTOR_LEN        = 5,
+              A_CELL_WIDTH      = 8,
+              B_CELL_WIDTH      = 8,
+              RESULT_CELL_WIDTH = 8,
+              TILING            = 8;
 
 	// Inputs
 	reg clk;
 	reg rst;
 	reg start;
-	reg [39:0] a;
-	reg [39:0] b;
+    reg [VECTOR_LEN*A_CELL_WIDTH-1:0] a;
+    reg [VECTOR_LEN*B_CELL_WIDTH-1:0] b;
 
 	// Outputs
-	wire [44:0] result;
-	wire finish;
-    
+	wire [VECTOR_LEN*RESULT_CELL_WIDTH-1:0] result;
+	wire valid;
+
     // memory
-    wire [8:0] result_mem [0:4];
+    wire [RESULT_CELL_WIDTH-1:0] result_mem [0:VECTOR_LEN-1];
     genvar i;
     generate
-    for (i=0; i<5; i=i+1) begin: MEM
-        assign result_mem[i] = result[i*9+:9];
+    for (i=0; i<VECTOR_LEN; i=i+1) begin: MEM
+        assign result_mem[i] = result[i*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH];
     end
     endgenerate
 
 	// Instantiate the Unit Under Test (UUT)
-	vector_subtract #(.TILING(2))
+	vector_subtract
+    #(
+        .VECTOR_LEN       (VECTOR_LEN       ),
+        .A_CELL_WIDTH     (A_CELL_WIDTH     ),
+        .B_CELL_WIDTH     (B_CELL_WIDTH     ),
+        .RESULT_CELL_WIDTH(RESULT_CELL_WIDTH),
+        .TILING           (TILING           ))
     uut (
 		.clk(clk), 
 		.rst(rst), 
@@ -53,7 +64,7 @@ module tb_vector_subtract;
 		.a(a), 
 		.b(b), 
 		.result(result), 
-		.finish(finish)
+		.valid(valid)
 	);
 
     always
@@ -64,8 +75,8 @@ module tb_vector_subtract;
 		clk = 0;
 		rst = 0;
 		start = 0;
-		a = {8'd50, 8'd40, 8'd30, 8'd20, 8'd10}; // 10, 20, 30, 40, 50
-		b = {8'd25, 8'd55, 8'd30, 8'd45, 8'd5};  // 5,  4,  3,  2,  1
+		a = {-8'd50, 8'd40, 8'd30, 8'd20, -8'd10}; // 10, 20, 30, 40, 50
+		b = {8'd1  , 8'd2 , -8'd3, 8'd4 , 8'd5};  // 5  , 4 , 3 , 2 , 1
 
         #20 rst = 1;
         #20 rst = 0;
@@ -75,6 +86,7 @@ module tb_vector_subtract;
 
         #40 start = 1;
         #2  start = 0;
+
 	end
       
 endmodule
--- backpropagation/vector_subtract.v
+++ backpropagation/vector_subtract.v
@@ -1,38 +1,34 @@
 module vector_subtract
 #(
-    parameter VECTOR_SIZE = 5,
-              CELL_WIDTH  = 8,
-              TILING      = 1
+    parameter VECTOR_LEN        = 5,
+              A_CELL_WIDTH      = 8,
+              B_CELL_WIDTH      = 8,
+              RESULT_CELL_WIDTH = 8,
+              TILING            = 1
 )(
-    input clk,
-    input rst,
-    input start,
-    input [VECTOR_SIZE*CELL_WIDTH-1:0] a,
-    input [VECTOR_SIZE*CELL_WIDTH-1:0] b,
-    output [VECTOR_SIZE*(CELL_WIDTH+1)-1:0] result,
-    output reg                              finish
+    input  clk,
+    input  rst,
+    input                                     start,
+    input  [VECTOR_LEN*A_CELL_WIDTH-1:0]      a,
+    input  [VECTOR_LEN*B_CELL_WIDTH-1:0]      b,
+    output [VECTOR_LEN*RESULT_CELL_WIDTH-1:0] result,
+    output                                    valid
 );
 
-    //define the log2 function
-    function integer log2;
-        input integer num;
-        integer i, result;
-        begin
-            for (i = 0; 2 ** i < num; i = i + 1)
-                result = i + 1;
-            log2 = result;
-        end
-    endfunction
+    `include "log2.v"
 
-    reg [VECTOR_SIZE*(CELL_WIDTH+1)-1:0] result_buffer;
-    reg [log2(VECTOR_SIZE):0]            counter;
+    reg [VECTOR_LEN*RESULT_CELL_WIDTH-1:0] result_buffer;
+    reg                                    valid_buffer;
+    reg [log2(VECTOR_LEN):0]               counter;
 
     // adders
-    wire [TILING*(CELL_WIDTH+1)-1:0] tiling_sum;
+    wire [RESULT_CELL_WIDTH-1:0] tiling_sum [TILING-1:0];
     genvar i;
     generate 
     for (i=0; i<TILING; i=i+1) begin: ADDERS
-        assign tiling_sum[i*(CELL_WIDTH+1)+:CELL_WIDTH+1] = $signed(a[(counter+i)*CELL_WIDTH+:CELL_WIDTH]) - $signed(b[(counter+i)*CELL_WIDTH+:CELL_WIDTH]);
+        assign tiling_sum[i] = 
+            $signed(a[(counter+i)*A_CELL_WIDTH+:A_CELL_WIDTH]) -
+            $signed(b[(counter+i)*B_CELL_WIDTH+:B_CELL_WIDTH]);
     end
     endgenerate
 
@@ -40,30 +36,34 @@ module vector_subtract
     localparam IDLE=0, RUN=1;
     reg state;
 
+    integer x;
+
     always @ (posedge clk) begin
         if (rst) begin
             counter       <= 0;
             result_buffer <= 0;
             state         <= IDLE;
-            finish        <= 0;
+            valid_buffer  <= 0;
         end
         else begin
             if (state == IDLE) begin
-                counter       <= 0;
-                state         <= start? RUN : IDLE;
-                finish        <= start? 0   : finish; // on start, reset finish
+                counter      <= 0;
+                state        <= start? RUN : IDLE;
+                valid_buffer <= start? 0   : valid_buffer; // on start, reset valid_buffer
             end
             else begin
-                result_buffer[counter*(CELL_WIDTH+1)+:TILING*(CELL_WIDTH+1)] <= tiling_sum;
-                if (counter >= VECTOR_SIZE - 1) begin
-                    counter       <= 0;
-                    state         <= IDLE;
-                    finish        <= 1;
+                for (x=0; x<TILING; x=x+1) begin: RES_MEM
+                    result_buffer[(counter+x)*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH] <= tiling_sum[x];
+                end
+                if (counter >= VECTOR_LEN - 1) begin
+                    counter      <= 0;
+                    state        <= IDLE;
+                    valid_buffer <= 1;
                 end
                 else begin
-                    counter <= counter + TILING;
-                    state   <= RUN;
-                    finish  <= 0;
+                    counter      <= counter + TILING;
+                    state        <= RUN;
+                    valid_buffer <= 0;
                 end
             end
         end
@@ -71,5 +71,6 @@ module vector_subtract
 
     //output
     assign result = result_buffer;
+    assign valid  = valid_buffer;
 
 endmodule
