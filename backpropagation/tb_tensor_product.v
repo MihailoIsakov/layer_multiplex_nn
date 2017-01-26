00a54999e9731a62baa73ef6c9b9d282a2c144b1
Mihailo Isakov <mihailo@protonmail.com>
2017-01-25 22:20:00 -0500

    better fixed point support

--- backpropagation/tb_tensor_product.v
+++ backpropagation/tb_tensor_product.v
@@ -24,37 +24,50 @@
 
 module tb_tensor_product;
 
-    parameter VECTOR_SIZE    = 5,
-              CELL_WIDTH     = 8,
-              TILING_H       = 1, // the number of cells from vector b processed each turn
-              TILING_V       = 2;  // the number of rows being processed each turn. 
+    parameter A_VECTOR_LEN      = 5, // length of vector a
+              B_VECTOR_LEN      = 5, // length of vector b
+              A_CELL_WIDTH      = 8, // width of the integer part of fixed point vector values of a
+              B_CELL_WIDTH      = 8, // width of the integer part of fixed point vector values of b
+              RESULT_CELL_WIDTH = 20, // width of the integer part of the result vector values
+              FRACTION_WIDTH    = 1, // width of the fraction of both a and b values
+              TILING_H          = 10, // the number of cells from vector b processed each turn
+              TILING_V          = 7; // the number of rows being processed each turn.
+
 	// Inputs
 	reg clk;
 	reg rst;
 	reg start;
-	reg [VECTOR_SIZE*CELL_WIDTH-1:0] a;
-	reg [VECTOR_SIZE*CELL_WIDTH-1:0] b;
+	reg [A_VECTOR_LEN*A_CELL_WIDTH-1:0] a;
+	reg [B_VECTOR_LEN*B_CELL_WIDTH-1:0] b;
 
     // wires to memory
-    wire [2*CELL_WIDTH-1:0] result_mem [VECTOR_SIZE-1:0][VECTOR_SIZE-1:0];
-    wire [2*CELL_WIDTH-1:0] result_vec [VECTOR_SIZE*VECTOR_SIZE-1:0];
+    wire [RESULT_CELL_WIDTH-1:0] result_mem [0:B_VECTOR_LEN-1][0:A_VECTOR_LEN-1];
+    //wire [2*RESULT_CELL_WIDTH-1:0] result_vec [B_VECTOR_LEN*A_VECTOR_SIZE-1:0];
     genvar i, j;
     generate
-    for (i=0; i<VECTOR_SIZE; i=i+1) begin
-        for (j=0; j<VECTOR_SIZE; j=j+1) begin
-            assign result_mem[i][j] = result[i*VECTOR_SIZE*2*CELL_WIDTH+j*2*CELL_WIDTH+:2*CELL_WIDTH];
-            assign result_vec[i*VECTOR_SIZE+j] = result[i*VECTOR_SIZE*2*CELL_WIDTH+j*2*CELL_WIDTH+:2*CELL_WIDTH];
+    for (i=0; i<B_VECTOR_LEN; i=i+1) begin
+        for (j=0; j<A_VECTOR_LEN; j=j+1) begin
+            assign result_mem[i][j] = result[i*B_VECTOR_LEN*RESULT_CELL_WIDTH+j*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH];
+            //assign result_vec[i*VECTOR_SIZE+j] = result[i*VECTOR_SIZE*2*CELL_WIDTH+j*2*CELL_WIDTH+:2*CELL_WIDTH];
         end
     end
     endgenerate
 
 	// Outputs
-	wire [VECTOR_SIZE*VECTOR_SIZE*2*CELL_WIDTH-1:0] result;
-	wire finish;
+	wire [A_VECTOR_LEN*B_VECTOR_LEN*RESULT_CELL_WIDTH-1:0] result;
+	wire valid;
 
 	// Instantiate the Unit Under Test (UUT)
 	tensor_product 
-    #(.VECTOR_SIZE(VECTOR_SIZE), .CELL_WIDTH(CELL_WIDTH), .TILING_H(TILING_H), .TILING_V(TILING_V))
+    #(
+        .A_VECTOR_LEN     (A_VECTOR_LEN     ),
+        .B_VECTOR_LEN     (B_VECTOR_LEN     ),
+        .A_CELL_WIDTH     (A_CELL_WIDTH     ),
+        .B_CELL_WIDTH     (B_CELL_WIDTH     ),
+        .FRACTION_WIDTH   (FRACTION_WIDTH   ),
+        .RESULT_CELL_WIDTH(RESULT_CELL_WIDTH),
+        .TILING_H         (TILING_H         ),
+        .TILING_V         (TILING_V         ))
     uut (
 		.clk(clk), 
 		.rst(rst), 
@@ -62,7 +75,7 @@ module tb_tensor_product;
 		.a(a), 
 		.b(b), 
 		.result(result), 
-		.finish(finish)
+		.valid(valid)
 	);
 
     always 
@@ -73,16 +86,14 @@ module tb_tensor_product;
 		clk = 0;
 		rst = 0;
 		start = 0;
-		a = {8'd50, 8'd40, 8'd30, 8'd20, 8'd10}; // 10, 20, 30, 40, 50
-		b = {8'd1,  8'd2,  8'd3,  8'd4,  8'd5};  // 5,  4,  3,  2,  1
+		a = {-8'd50 , 8'd40 , 8'd30 , 8'd20 , 8'd10}; // 10 , 20 , 30 , 40 , 50
+		b = {8'd1   , 8'd2  , -8'd3 , 8'd4  , 8'd5};  // 5  , 4  , 3  , 2  , 1
 
         #20  rst = 1;
         #20  rst = 0;
 
         #20 start = 1;
         #2  start = 0;
-
-
 
 	end
       
--- backpropagation/tensor_product.v
+++ backpropagation/tensor_product.v
@@ -1,44 +1,43 @@
 module tensor_product
 #(
-    parameter VECTOR_SIZE    = 5,
-              CELL_WIDTH     = 8,
-              TILING_H       = 4, // the number of cells from vector b processed each turn
-              TILING_V       = 1  // the number of rows being processed each turn. 
+    parameter A_VECTOR_LEN         = 5, // length of vector a
+              B_VECTOR_LEN         = 5, // length of vector b
+              A_CELL_WIDTH         = 8, // width of the integer part of fixed point vector values of a
+              B_CELL_WIDTH         = 8, // width of the integer part of fixed point vector values of b
+              RESULT_CELL_WIDTH    = 8, // width of the integer part of the result vector values
+              FRACTION_WIDTH       = 4, // width of the fraction of both a and b values
+              TILING_H             = 1, // the number of cells from vector b processed each turn
+              TILING_V             = 1  // the number of rows being processed each turn.
 )(
     input clk,
     input rst,
-    input start,
-    input [VECTOR_SIZE*CELL_WIDTH-1:0] a,
-    input [VECTOR_SIZE*CELL_WIDTH-1:0] b,
-    output [VECTOR_SIZE*VECTOR_SIZE*2*CELL_WIDTH-1:0] result,
-    output reg                                        finish
+    input                                                    start,
+    input  [A_VECTOR_LEN*A_CELL_WIDTH-1:0]                   a,
+    input  [B_VECTOR_LEN*B_CELL_WIDTH-1:0]                   b,
+    output [A_VECTOR_LEN*B_VECTOR_LEN*RESULT_CELL_WIDTH-1:0] result,
+    output                                                   valid
 );
 
-    //define the log2 function
-    function integer log2;
-        input integer num;
-        integer i, result;
-        begin
-            for (i = 0; 2 ** i < num; i = i + 1)
-                result = i + 1;
-            log2 = result;
-        end
-    endfunction
+    `include "log2.v"
+    localparam MSB = RESULT_CELL_WIDTH - 1;
 
-    reg  [log2(VECTOR_SIZE)-1:0]              counter_h, counter_v; // size of counter can be optimized, should be VECTOR_SIZE/TILING rounded up
-    wire [TILING_V*CELL_WIDTH-1:0]            tile_a;
-    wire [TILING_H*CELL_WIDTH-1:0]            tile_b;
-    wire [TILING_H*2*CELL_WIDTH-1:0] tile_product [TILING_V-1:0];
-    reg  [VECTOR_SIZE*2*CELL_WIDTH-1:0] result_buffer [VECTOR_SIZE-1:0];
+    reg  [log2(A_VECTOR_LEN > B_VECTOR_LEN ? A_VECTOR_LEN : B_VECTOR_LEN):0] counter_h, counter_v; // size of counter can be optimized, should be VECTOR_SIZE/TILING rounded up
+    reg  [RESULT_CELL_WIDTH-1:0]     result_buffer [B_VECTOR_LEN-1:0][A_VECTOR_LEN-1:0];
+    reg  valid_buffer;
+    wire [TILING_V*A_CELL_WIDTH-1:0] tile_a;
+    wire [TILING_H*B_CELL_WIDTH-1:0] tile_b;
+    wire [RESULT_CELL_WIDTH-1:0]     tile_product [TILING_H-1:0][TILING_V-1:0]; // FIXME the second index controls row?!?
 
-    assign tile_a = a[counter_v*TILING_V*CELL_WIDTH+:TILING_V*CELL_WIDTH];
-    assign tile_b = b[counter_h*TILING_H*CELL_WIDTH+:TILING_H*CELL_WIDTH];
+    assign tile_a = a[counter_v*TILING_V*A_CELL_WIDTH+:TILING_V*A_CELL_WIDTH];
+    assign tile_b = b[counter_h*TILING_H*B_CELL_WIDTH+:TILING_H*B_CELL_WIDTH];
 
     genvar i, j;
     generate
     for (i=0; i<TILING_V; i=i+1) begin: HEIGHT_TILING
         for (j=0; j<TILING_H; j=j+1) begin: WIDTH_TILING
-            assign tile_product[i][j*2*CELL_WIDTH+:2*CELL_WIDTH] = tile_a[i*CELL_WIDTH+:CELL_WIDTH] * tile_b[j*CELL_WIDTH+:CELL_WIDTH];
+            // concatenate on the spot
+            assign tile_product[j][i] = 
+                ($signed(tile_a[i*A_CELL_WIDTH+:A_CELL_WIDTH]) * $signed(tile_b[j*B_CELL_WIDTH+:B_CELL_WIDTH])) >>> FRACTION_WIDTH;
         end 
     end
     endgenerate
@@ -51,15 +50,15 @@ module tensor_product
         if (rst) begin
             counter_h <= 0;
             counter_v <= 0;
-            finish    <= 0;
+            valid_buffer <= 0;
             state     <= IDLE;
         end
         else begin
             if (state == RUN)
-                if ((counter_h + 1) * TILING_H >= VECTOR_SIZE) begin // go one row down
-                    if ((counter_v + 1) * TILING_V >= VECTOR_SIZE) begin
-                        finish <= 1;
-                        state  <= IDLE;
+                if ((counter_h + 1) * TILING_H >= B_VECTOR_LEN) begin // go one row down
+                    if ((counter_v + 1) * TILING_V >= A_VECTOR_LEN) begin
+                        valid_buffer <= 1;
+                        state         <= IDLE;
                     end else begin
                         counter_v <= counter_v + 1;
                         counter_h <= 0;
@@ -68,21 +67,26 @@ module tensor_product
                     counter_h <= counter_h + 1;
                 end
             else if (state == IDLE) begin
-                counter_h <= 0;
-                counter_v <= 0;
-                finish    <= finish;
-                state     <= start; // 1 if start 
+                counter_h     <= 0;
+                counter_v     <= 0;
+                valid_buffer <= valid_buffer;
+                state         <= start; // 1 if start
             end
         end
     end
 
     // saving tile products into the result buffer
-    genvar k;
+    genvar k, l;
     generate
         for (k=0; k<TILING_V; k=k+1) begin: RESULT_V
-            always @ (posedge clk) begin
-                if (state == RUN)
-                    result_buffer[counter_v*TILING_V+k][counter_h*TILING_H*2*CELL_WIDTH+:TILING_H*2*CELL_WIDTH] <= tile_product[k];
+            for (l=0; l<TILING_H; l=l+1) begin: RESULT_H
+                always @ (posedge clk) begin
+                    if (state == RUN) begin
+                        // this is where the truncating of result happens
+                        //result_buffer[counter_v*TILING_V+k][counter_h*TILING_H*RESULT_CELL_WIDTH+:TILING_H*RESULT_CELL_WIDTH] <= tile_product[k];
+                        result_buffer[counter_v*TILING_V+k][counter_h*TILING_H+l] <= tile_product[l][k];
+                    end
+                end
             end
         end
     endgenerate
@@ -90,12 +94,12 @@ module tensor_product
 
     // outputs
     generate 
-    for (i=0; i<VECTOR_SIZE; i=i+1) begin: OUTPUT_RESULT_V
-        for (j=0; j<VECTOR_SIZE; j=j+1) begin: OUTPUT_RESULT_H
-            assign result[i*VECTOR_SIZE*2*CELL_WIDTH+j*2*CELL_WIDTH+:2*CELL_WIDTH] = result_buffer[i][j*2*CELL_WIDTH+:2*CELL_WIDTH];
+    for (i=0; i<A_VECTOR_LEN; i=i+1) begin: OUTPUT_RESULT_V
+        for (j=0; j<B_VECTOR_LEN; j=j+1) begin: OUTPUT_RESULT_H
+            assign result[i*B_VECTOR_LEN*RESULT_CELL_WIDTH+j*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH] = result_buffer[i][j];
         end
     end
     endgenerate
-
+    assign valid = valid_buffer;
 
 endmodule
