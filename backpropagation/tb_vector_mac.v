cac452e3dec7ad21592f9a26ae47d61a0e90c96f
Mihailo Isakov <mihailo@protonmail.com>
2017-01-29 22:10:27 -0500

    vector mac has overflow line and performs truncating. Cleaner tests.

--- backpropagation/tb_vector_mac.v
+++ backpropagation/tb_vector_mac.v
@@ -24,26 +24,40 @@
 
 module tb_vector_mac;
 
+    parameter VECTOR_LEN        = 5,  // number of elements in the vectors
+              A_CELL_WIDTH      = 8,  // width of elements in the vector a
+              B_CELL_WIDTH      = 8,  // width of elements in the vector b
+              RESULT_CELL_WIDTH = 10, // width of elements in the output vector
+              TILING            = 2;  // number of mults generated for dot product
+
 	// Inputs
 	reg clk;
 	reg rst;
 	reg start;
-	reg [39:0] a;
-	reg [39:0] b;
+	reg [VECTOR_LEN*A_CELL_WIDTH-1:0] a;
+	reg [VECTOR_LEN*B_CELL_WIDTH-1:0] b;
 
 	// Outputs
-	wire [8+8+4-1:0] result;
+	wire [RESULT_CELL_WIDTH-1:0] result;
 	wire valid;
+    wire error;
 
 	// Instantiate the Unit Under Test (UUT)
-	vector_mac uut (
+    vector_mac #(
+       .VECTOR_LEN       (VECTOR_LEN       ), 
+       .A_CELL_WIDTH     (A_CELL_WIDTH     ), 
+       .B_CELL_WIDTH     (B_CELL_WIDTH     ), 
+       .RESULT_CELL_WIDTH(RESULT_CELL_WIDTH), 
+       .TILING           (TILING           )
+    ) uut (
 		.clk(clk), 
 		.rst(rst), 
 		.start(start), 
 		.a(a), 
 		.b(b), 
 		.result(result), 
-		.valid(valid)
+		.valid(valid),
+        .error(error)
 	);
 
     always 
@@ -51,18 +65,23 @@ module tb_vector_mac;
 
 	initial begin
 		// Initialize Inputs
-		clk = 0;
-		rst = 0;
+		clk   = 0;
+		rst   = 0;
 		start = 0;
-		a = 0;
-		b = 0;
+		a     = 0;
+		b     = 0;
 
         #20 rst = 1;
         #2  rst = 0;
 
         #20 start = 1;
-            a = {8'd10, -8'd20, 8'd1, 8'd100, 8'd0};
-            b = {8'd5,   8'd4,  8'd3, 8'd2,   8'd1};
+            a     = {8'd10, -8'd20, 8'd1, 8'd100, 8'd0};
+            b     = {8'd5,   8'd4,  8'd3, 8'd2,   8'd1};
+        #2  start = 0;
+
+        #20 start = 1;
+            a     = {8'd10, -8'd200, 8'd100, 8'd100, 8'd0};
+            b     = {8'd50,   8'd4,  8'd30, 8'd2,   8'd1};
         #2  start = 0;
 
 	end
--- backpropagation/vector_mac.v
+++ backpropagation/vector_mac.v
@@ -1,39 +1,31 @@
 module vector_mac
 #(
-    parameter VECTOR_SIZE = 5, // number of elements in the vectors
-              WIDTH1      = 8, // width of elements in the vector a
-              WIDTH2      = 8, // width of elements in the vector b
-              TILING      = 2  // number of mults generated for dot
-                               // please keep comment lengths syncd
+    parameter VECTOR_LEN        = 5, // number of elements in the vectors
+              A_CELL_WIDTH      = 8, // width of elements in the vector a
+              B_CELL_WIDTH      = 8, // width of elements in the vector b
+              RESULT_CELL_WIDTH = 8, // width of elements in the output vector
+              TILING            = 2  // number of mults generated for dot product
 )(
     input clk,
     input rst,
-    input                          start,
-    input [VECTOR_SIZE*WIDTH1-1:0] a,
-    input [VECTOR_SIZE*WIDTH2-1:0] b,
-    output [RESULT_WIDTH-1:0]      result,
-    output                         valid
+    input                                start,
+    input  [VECTOR_LEN*A_CELL_WIDTH-1:0] a,
+    input  [VECTOR_LEN*B_CELL_WIDTH-1:0] b,
+    output [RESULT_CELL_WIDTH-1:0]       result,
+    output                               valid,
+    output                               error
 );
 
-    //define the log2 function
-    function integer log2;
-        input integer num;
-        integer i, result;
-        begin
-            for (i = 0; 2 ** i < num; i = i + 1)
-                result = i + 1;
-            log2 = result;
-        end
-    endfunction
+    `include "log2.v"
     
     // width needed to store the multiplication
-    localparam MULT_WIDTH   = WIDTH1 + WIDTH2;
+    localparam MULT_WIDTH   = A_CELL_WIDTH + B_CELL_WIDTH;
     // result size accomodating for the sum and +1 for log2 rounding up
-    localparam RESULT_WIDTH = WIDTH1 + WIDTH2 + log2(VECTOR_SIZE) + 1; 
+    localparam AB_SUM_WIDTH = A_CELL_WIDTH + B_CELL_WIDTH + log2(VECTOR_LEN) + 1; 
 
-    reg signed [RESULT_WIDTH-1:0] result_buffer, sum;
+    reg signed [AB_SUM_WIDTH-1:0] result_buffer, sum;
     reg valid_buffer;
-    reg [log2(VECTOR_SIZE):0] counter; // +1 to round up log2
+    reg [log2(VECTOR_LEN):0] counter; // +1 to round up log2
 
     // FIXME The current reduce sum operation is serial - the vector is summed up one element at a time combinatorially.
     // This may create a critical path, and should be replaced with an adder tree
@@ -43,8 +35,8 @@ module vector_mac
     always @ (posedge clk) begin
         sum = 0;
         for (i=0; i<TILING; i=i+1) begin: MULTIPLIERS
-            if (counter+i<VECTOR_SIZE)
-                sum = sum + $signed(a[(counter+i)*WIDTH1+:WIDTH1]) * $signed(b[(counter+i)*WIDTH2+:WIDTH2]);
+            if (counter+i<VECTOR_LEN)
+                sum = sum + $signed(a[(counter+i)*A_CELL_WIDTH+:A_CELL_WIDTH]) * $signed(b[(counter+i)*B_CELL_WIDTH+:B_CELL_WIDTH]);
         end
     end
     //endgenerate
@@ -66,11 +58,11 @@ module vector_mac
                 counter       <= 0;
                 state         <= start ? RUN : IDLE;
                 valid_buffer  <= start ? 0   : valid_buffer; // on start, reset valid_buffer
-                result_buffer = 0;
+                result_buffer = start  ? 0   : result_buffer;
             end
             else begin
                 result_buffer = result_buffer + sum;
-                if (counter >= VECTOR_SIZE - 1) begin
+                if (counter >= VECTOR_LEN - 1) begin
                     counter       <= 0;
                     state         <= IDLE;
                     valid_buffer  <= 1;
@@ -85,7 +77,11 @@ module vector_mac
     end
 
     //output
-    assign result = result_buffer;
+    assign result = result_buffer[RESULT_CELL_WIDTH-1:0];  // truncated
     assign valid  = valid_buffer && ~start;
+    // checks if the truncated part is not all zeros of ones
+    assign error  = 
+        (|result_buffer[AB_SUM_WIDTH-1:RESULT_CELL_WIDTH] == 1) &&  // does not work for all 1s
+        (|(~result_buffer[AB_SUM_WIDTH-1:RESULT_CELL_WIDTH]) == 1); // does not work for all 0s
 
 endmodule
