15558cc7c29ae3187c53ae400c6a3bca9d632342
Mihailo Isakov <mihailo@protonmail.com>
2017-02-01 17:07:52 -0500

    test seems to be working

--- backpropagation/error_propagator.v
+++ backpropagation/error_propagator.v
@@ -1,91 +1,86 @@
 module error_propagator
 #(
-    parameter MATRIX_WIDTH      = 4,  // width of the weight matrix aka the number of columns
-              MATRIX_HEIGHT     = 5,  // height of the weight matrix aka the number of rows and size of vector
-              VECTOR_CELL_WIDTH = 8,  // width of each vector cell in bits
-              MATRIX_CELL_WIDTH = 8,  // widht of each matrix cell in bits
-              NEURON_ADDR_WIDTH = 10, // activations from neurons before the sigmoid
-              ACTIVATION_WIDTH  = 9,  // width after sigmoid
-              TILING_ROW        = 3,  // number of vector_mac units to create
-              TILING_COL        = 3   // number of multipliers per vector_mac unit
+    parameter MATRIX_WIDTH       = 4,  // width of the weight matrix aka the number of columns
+              MATRIX_HEIGHT      = 5,  // height of the weight matrix aka the number of rows and size of vector
+              DELTA_CELL_WIDTH   = 8,  // width of each delta cell in bits
+              WEIGHTS_CELL_WIDTH = 8,  // widht of each matrix cell in bits
+              NEURON_ADDR_WIDTH  = 10, // width of activations from neurons before the sigmoid
+              ACTIVATION_WIDTH   = 9,  // cell width after sigmoid
+              FRACTION_WIDTH     = 4,
+              TILING_ROW         = 3,  // number of vector_mac units to create
+              TILING_COL         = 3   // number of multipliers per vector_mac unit
 )(
     input clk,
     input rst,
-    input                                                     start,
-    input  [VECTOR_WIDTH*VECTOR_CELL_WIDTH-1:0]               delta_input,
-    input  [MATRIX_WIDTH*NEURON_ADDR_WIDTH-1:0]               z,
-    input  [MATRIX_WIDTH*MATRIX_HEIGHT*MATRIX_CELL_WIDTH-1:0] w,
-    output [MATRIX_WIDTH*RESULT_WIDTH-1:0]               delta_output,
-    output                                                    valid
+    input                                                      start,
+    input  [MATRIX_HEIGHT*DELTA_CELL_WIDTH-1:0]                delta_input,
+    input  [MATRIX_WIDTH*NEURON_ADDR_WIDTH-1:0]                z,
+    input  [MATRIX_WIDTH*MATRIX_HEIGHT*WEIGHTS_CELL_WIDTH-1:0] w,
+    output [MATRIX_WIDTH*DELTA_CELL_WIDTH-1:0]                 delta_output,
+    output                                                     valid,
+    output                                                     error
 );
 
-    //define the log2 function
-    function integer log2;
-        input integer num;
-        integer i, result;
-        begin
-            for (i = 0; 2 ** i < num; i = i + 1)
-                result = i + 1;
-            log2 = result;
-        end
-    endfunction
-    //
+    `include "log2.v"
 
-    localparam VECTOR_WIDTH = MATRIX_HEIGHT;
-    localparam MVM_RESULT_WIDTH = VECTOR_CELL_WIDTH + MATRIX_CELL_WIDTH + log2(VECTOR_WIDTH) + 1;
-    localparam RESULT_WIDTH = MVM_RESULT_WIDTH + ACTIVATION_WIDTH;
-
-    wire [MATRIX_WIDTH*MVM_RESULT_WIDTH-1:0] mvm_result;
+    reg inner_start, dot_start; // inner start starts both the MVM and the sigmoid
+    wire [MATRIX_WIDTH*DELTA_CELL_WIDTH-1:0] mvm_result;
     wire [MATRIX_WIDTH*ACTIVATION_WIDTH-1:0] lut_result;
-    wire [MATRIX_WIDTH*RESULT_WIDTH-1:0]     dot_result;
+    wire [MATRIX_WIDTH*DELTA_CELL_WIDTH-1:0] dot_result;
     wire mvm_valid, lut_valid, dot_valid;
-    reg inner_start, dot_start;
+    wire mvm_error, dot_error;
 
     mvm #(
-        .MATRIX_WIDTH     (MATRIX_WIDTH     ),
-        .MATRIX_HEIGHT    (MATRIX_HEIGHT    ),
-        .VECTOR_CELL_WIDTH(VECTOR_CELL_WIDTH),
-        .MATRIX_CELL_WIDTH(MATRIX_CELL_WIDTH),
-        .TILING_ROW       (TILING_ROW       ),
-        .TILING_COL       (TILING_COL       )) 
+        .MATRIX_WIDTH     (MATRIX_WIDTH      ),
+        .MATRIX_HEIGHT    (MATRIX_HEIGHT     ),
+        .VECTOR_CELL_WIDTH(DELTA_CELL_WIDTH  ),
+        .MATRIX_CELL_WIDTH(WEIGHTS_CELL_WIDTH),
+        .RESULT_CELL_WIDTH(DELTA_CELL_WIDTH  ),
+        .FRACTION_WIDTH   (FRACTION_WIDTH    ),
+        .TILING_ROW       (TILING_ROW        ),
+        .TILING_COL       (TILING_COL        )) 
     mvm(
-        .clk(clk),
-        .rst(rst),
-        .start(inner_start),
+        .clk   (clk        ),
+        .rst   (rst        ),
+        .start (inner_start),
         .vector(delta_input),
-        .matrix(w),
-        .result(mvm_result),
-        .valid(mvm_valid)
+        .matrix(w          ),
+        .result(mvm_result ),
+        .valid (mvm_valid  ),
+        .error (mvm_error  )
     );
 
     lut #(
-        .NEURON_NUM(MATRIX_WIDTH),
-        .LUT_ADDR_SIZE(NEURON_ADDR_WIDTH),
-        .LUT_DEPTH(1 << NEURON_ADDR_WIDTH),
-        .LUT_WIDTH(ACTIVATION_WIDTH),
-        .LUT_INIT_FILE("activations.list")
+        .NEURON_NUM   (MATRIX_WIDTH          ),
+        .LUT_ADDR_SIZE(NEURON_ADDR_WIDTH     ),
+        .LUT_DEPTH    (1 << NEURON_ADDR_WIDTH),
+        .LUT_WIDTH    (ACTIVATION_WIDTH      ),
+        .LUT_INIT_FILE("activations.list"    )
     ) sigmoid (
-        .clk(clk),
-        .rst(rst),
-        .start(inner_start),
-        .inputs(z),
-        .outputs(lut_result),
-        .valid(lut_valid)
+        .clk    (clk        ),
+        .rst    (rst        ),
+        .start  (inner_start),
+        .inputs (z          ),
+        .outputs(lut_result ),
+        .valid  (lut_valid  )
     );
 
-    vector_dot2 #(
-        .VECTOR_SIZE(MATRIX_WIDTH),
-        .CELL_A_WIDTH(MVM_RESULT_WIDTH),
-        .CELL_B_WIDTH(ACTIVATION_WIDTH),
-        .TILING(2)
+    vector_dot #(
+        .VECTOR_LEN       (MATRIX_WIDTH    ),
+        .A_CELL_WIDTH     (DELTA_CELL_WIDTH),
+        .B_CELL_WIDTH     (ACTIVATION_WIDTH),
+        .RESULT_CELL_WIDTH(DELTA_CELL_WIDTH),
+        .FRACTION_WIDTH   (FRACTION_WIDTH  ),
+        .TILING           (2               )
     ) dot (
-        .clk(clk),
-        .rst(rst),
-        .start(dot_start),
-        .a(mvm_result),
-        .b(lut_result),
+        .clk   (clk       ),
+        .rst   (rst       ),
+        .start (dot_start ),
+        .a     (mvm_result),
+        .b     (lut_result),
         .result(dot_result),
-        .valid(dot_valid)
+        .valid (dot_valid ),
+        .error (dot_error )
     );
 
     localparam IDLE=0, MVM=1, DOT=2;
@@ -126,5 +121,22 @@ module error_propagator
     // outputs
     assign delta_output = dot_result;
     assign valid        = dot_valid;
+    assign error        = mvm_error | dot_error;
+
+    // testing
+    wire [ACTIVATION_WIDTH-1:0] activation_mem [MATRIX_WIDTH-1:0];
+    wire [DELTA_CELL_WIDTH-1:0] mvm_mem        [MATRIX_WIDTH-1:0];
+    wire [DELTA_CELL_WIDTH-1:0] dot_mem        [MATRIX_WIDTH-1:0];
+    wire [DELTA_CELL_WIDTH-1:0] delta_mem      [MATRIX_WIDTH-1:0];
+
+    genvar i;
+    generate
+    for (i=0; i<MATRIX_WIDTH; i=i+1) begin: MEM
+        assign activation_mem[i] = lut_result[i*ACTIVATION_WIDTH+:ACTIVATION_WIDTH];
+        assign mvm_mem[i]        = mvm_result[i*DELTA_CELL_WIDTH+:DELTA_CELL_WIDTH];
+        assign dot_mem[i]        = dot_result[i*DELTA_CELL_WIDTH+:DELTA_CELL_WIDTH];
+        assign delta_mem[i]      = delta_output[i*DELTA_CELL_WIDTH+:DELTA_CELL_WIDTH];
+    end
+    endgenerate
     
 endmodule
