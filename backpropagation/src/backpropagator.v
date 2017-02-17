288dcdaaf9a3bc31815bb1bf24728dbbeaf51997
Mihailo Isakov <mihailo@protonmail.com>
2017-02-17 12:00:43 -0500

    initial backpropagator, green but not working

--- backpropagation/src/backpropagator.v
+++ backpropagation/src/backpropagator.v
@@ -1,66 +1,168 @@
 module backpropagator
 #(
-    parameter NEURON_NUM = 10,
+    parameter NEURON_NUM          = 5,  // number of cells in the vectors a and delta
               NEURON_OUTPUT_WIDTH = 10, // size of the output of the neuron (z signal)
               ACTIVATION_WIDTH    = 9,  // size of the neurons activation
-              LAYER_ADDR_WIDTH    = 2,  // size of the layer number 
+              DELTA_CELL_WIDTH    = 10, // width of each delta cell
+              WEIGHT_CELL_WIDTH   = 16, // width of individual weights
+              FRACTION_WIDTH      = 0,
+              LAYER_ADDR_WIDTH    = 2,
               LAYER_MAX           = 3,  // number of layers in the network
               SAMPLE_ADDR_SIZE    = 10, // size of the sample addresses
-              TARGET_FILE         = "targets.list"
+              TARGET_FILE         = "targets.list",
+              WEIGHT_INIT_FILE    = "weight_init.list"
 
 )(
     input clk,
     input rst,
-    input start,
-    input [LAYER_ADDR_WIDTH-1:0]               current_layer,
-    input [SAMPLE_ADDR_SIZE-1:0]               sample,
-    input [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0] z,
-    input [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0] z_prev,
-
+    input                                                start,
+    input [LAYER_ADDR_WIDTH-1:0]                         current_layer,
+    input [SAMPLE_ADDR_SIZE-1:0]                         sample,
+    input [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0]           z,
+    input [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0]           z_prev,
+    output [NEURON_NUM*NEURON_NUM*WEIGHT_CELL_WIDTH-1:0] weights,
+    output                                               valid,
+    output                                               error
 );
 
-    localparam DELTA_WIDTH = 2*(1+ACTIVATION_WIDTH);
+    reg [NEURON_NUM*NEURON_NUM*WEIGHT_CELL_WIDTH-1:0] weights_buffer;
+    reg valid_buffer;
 
-    reg fetcher_start, prop_start;
+    reg wc_start, ef_start, ep_start; 
+    wire [NEURON_NUM*DELTA_CELL_WIDTH-1:0] delta, delta_prev;
+    wire [NEURON_NUM*NEURON_NUM*WEIGHT_CELL_WIDTH-1:0] w;
+    wire ef_valid, ep_valid, wc_valid, wc_error, ef_error, ep_error;
 
-    wire [NEURON_NUM*DELTA_WIDTH-1:0] fetcher_delta, prop_delta;
-    wire                              fetcher_delta_valid, prop_delta_valid;
+    weight_controller #(
+        .NEURON_NUM         (NEURON_NUM         ),
+        .NEURON_OUTPUT_WIDTH(NEURON_OUTPUT_WIDTH),
+        .ACTIVATION_WIDTH   (ACTIVATION_WIDTH   ),
+        .DELTA_CELL_WIDTH   (DELTA_CELL_WIDTH   ),
+        .WEIGHT_CELL_WIDTH  (WEIGHT_CELL_WIDTH  ),
+        .LAYER_ADDR_WIDTH   (LAYER_ADDR_WIDTH   ),
+        .FRACTION_WIDTH     (FRACTION_WIDTH     ),
+        .WEIGHT_INIT_FILE   (WEIGHT_INIT_FILE   )
+    ) weight_controller (
+        .clk  (clk          ),
+        .rst  (rst          ),
+        .start(wc_start     ),
+        .z    (z_prev       ),
+        .delta(delta        ),
+        .layer(current_layer),
+        .w    (w            ),
+        .valid(wc_valid     ),
+        .error(wc_error     )
+    );
+
 
     error_fetcher #(
-        NEURON_NUM         (NEURON_NUM         ),
-        NEURON_OUTPUT_WIDTH(NEURON_OUTPUT_WIDTH),
-        ACTIVATION_WIDTH   (ACTIVATION_WIDTH   ),
-        LAYER_ADDR_WIDTH   (LAYER_ADDR_WIDTH   ),
-        LAYER_MAX          (LAYER_MAX          ),
-        SAMPLE_ADDR_SIZE   (SAMPLE_ADDR_SIZE   ),
-        TARGET_FILE        (TARGET_FILE        )
-    ) error_fetcher(
-        .clk(clk),
-        .rst(rst),
-        .start(fetcher_start),
-        .layer(current_layer),
-        .sample_index(sample),
-        .z(z),
-        .delta_input(prop_delta),
-        .delta_input_valid(prop_delta_valid),
-        .delta_output(fetcher_delta),
-        .delta_output_valid(fetcher_delta_valid)
+        .NEURON_NUM         (NEURON_NUM         ),
+        .NEURON_OUTPUT_WIDTH(NEURON_OUTPUT_WIDTH),
+        .DELTA_CELL_WIDTH   (DELTA_CELL_WIDTH   ),
+        .ACTIVATION_WIDTH   (ACTIVATION_WIDTH   ),
+        .FRACTION_WIDTH     (FRACTION_WIDTH     ),
+        .LAYER_ADDR_WIDTH   (LAYER_ADDR_WIDTH   ),
+        .LAYER_MAX          (LAYER_MAX          ),
+        .SAMPLE_ADDR_SIZE   (SAMPLE_ADDR_SIZE   ),
+        .TARGET_FILE        (TARGET_FILE        )
+    ) error_fetcher (
+        .clk               (clk          ),
+        .rst               (rst          ),
+        .start             (ef_start     ),
+        .layer             (current_layer),
+        .sample_index      (sample       ),
+        .z                 (z            ),
+        .delta_input       (delta_prev   ),
+        .delta_input_valid (ep_valid     ),
+        .delta_output      (delta        ),
+        .delta_output_valid(ef_valid     ),
+        .error             (ef_error     )
     );
+
 
     error_propagator #(
-        .MATRIX_WIDTH(NEURON_NUM),
-        .MATRIX_HEIGHT(NEURON_NUM),
-        .VECTOR_CELL_WIDTH()
-        .MATRIX_CELL_WIDTH
-        .NEURON_ADDR_WIDTH
-        .ACTIVATION_WIDTH 
-        .TILING_ROW       
-        .TILING_COL       
-    ) propagator (
-        .clk(clk),
-        .rst(rst),
-        .start(prop_start),
-
+        .MATRIX_WIDTH      (NEURON_NUM         ),
+        .MATRIX_HEIGHT     (NEURON_NUM         ),
+        .DELTA_CELL_WIDTH  (DELTA_CELL_WIDTH   ),
+        .WEIGHTS_CELL_WIDTH(WEIGHT_CELL_WIDTH  ),
+        .NEURON_ADDR_WIDTH (NEURON_OUTPUT_WIDTH),
+        .ACTIVATION_WIDTH  (ACTIVATION_WIDTH   ),
+        .FRACTION_WIDTH    (FRACTION_WIDTH     ),
+        .TILING_ROW        (2                  ),
+        .TILING_COL        (2                  )
+    ) error_propagator (
+        .clk         (clk       ),
+        .rst         (rst       ),
+        .start       (ep_start  ),
+        .delta_input (delta     ),
+        .z           (z_prev    ),
+        .w           (w         ),
+        .delta_output(delta_prev),
+        .valid       (ep_valid  ),
+        .error       (ep_error  )
     );
 
+    localparam IDLE = 0, FETCH=1, PROPAGATE=2, UPDATE=3;
+    reg [1:0] state;
+
+    always @ (posedge clk) begin
+        if (rst) begin
+            state          <= IDLE;
+            wc_start       <= 0;
+            ef_start       <= 0;
+            ep_start       <= 0;
+            // output buffers
+            valid_buffer   <= 0;
+            weights_buffer <= 0;
+        end
+        else begin
+            if (wc_valid)
+                weights_buffer <= w;
+
+            case (state)
+                IDLE: begin
+                    state        <= start ? FETCH : IDLE;
+                    ef_start     <= start ? 1     : 0;
+                    ep_start     <= 0;
+                    wc_start     <= 0;
+                    valid_buffer <= start ? 0 : valid_buffer;
+                end
+                FETCH: begin
+                    state        <= ef_valid ? PROPAGATE : FETCH;
+                    ef_start     <= 0;
+                    ep_start     <= ef_valid ? 1         : 0;
+                    wc_start     <= 0;
+                    valid_buffer <= 0;
+                end
+                PROPAGATE: begin
+                    state        <= ep_valid ? UPDATE : PROPAGATE;
+                    ef_start     <= 0;
+                    ep_start     <= 0;
+                    wc_start     <= ep_valid ? 1      : 0;
+                    valid_buffer <= 0;
+                end
+                UPDATE: begin
+                    state        <= wc_valid ? IDLE : UPDATE;
+                    ef_start     <= 0;
+                    ep_start     <= 0;
+                    wc_start     <= 0;
+                    valid_buffer <= wc_valid ? 1    : 0;
+                end
+                default: begin
+                    state        <= IDLE;
+                    ef_start     <= 0;
+                    ep_start     <= 0;
+                    wc_start     <= 0;
+                    valid_buffer <= 0;
+                end
+            endcase
+        end
+    end
+
+    // outputs 
+    assign weights = weights_buffer;
+    assign valid = valid_buffer;
+    assign error = ef_error | ep_error | wc_error;
+
 endmodule
+
---
+++ backpropagation/src/testbenches/tb_backpropagator.v
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:12:49 02/16/2017
+// Design Name:   backpropagator
+// Module Name:   /home/mihailo/development/projects/layer_multiplex_nn/backpropagation/tb_backpropagator.v
+// Project Name:  backpropagation
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: backpropagator
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tb_backpropagator;
+
+    parameter NEURON_NUM          = 5,  // number of cells in the vectors a and delta
+              NEURON_OUTPUT_WIDTH = 10, // size of the output of the neuron (z signal)
+              ACTIVATION_WIDTH    = 9,  // size of the neurons activation
+              DELTA_CELL_WIDTH    = 10, // width of each delta cell
+              WEIGHT_CELL_WIDTH   = 16, // width of individual weights
+              FRACTION_WIDTH      = 0,
+              LAYER_ADDR_WIDTH    = 2,
+              LAYER_MAX           = 3,  // number of layers in the network
+              SAMPLE_ADDR_SIZE    = 10, // size of the sample addresses
+              TARGET_FILE         = "targets.list",
+              WEIGHT_INIT_FILE    = "weight_init.list";
+
+	// Inputs
+	reg clk;
+	reg rst;
+    reg                                               start;
+    reg [LAYER_ADDR_WIDTH-1:0]                        current_layer;
+    reg [SAMPLE_ADDR_SIZE-1:0]                        sample;
+    reg [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0]          z;
+    reg [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0]          z_prev;
+    
+	// Outputs
+    wire [NEURON_NUM*NEURON_NUM*WEIGHT_CELL_WIDTH-1:0] weights;
+    wire                                               valid;
+    wire                                               error;
+
+	// Instantiate the Unit Under Test (UUT)
+	backpropagator uut (
+		.clk(clk), 
+		.rst(rst), 
+		.start(start), 
+		.current_layer(current_layer), 
+		.sample(sample), 
+		.z(z), 
+		.z_prev(z_prev), 
+		.weights(weights), 
+		.valid(valid), 
+		.error(error)
+	);
+
+    always 
+        #1 clk = ~clk;
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		start = 0;
+		current_layer = LAYER_MAX-1;
+		sample = 0;
+		z = {10'd300, 10'd400, 10'd500, 10'd600, 10'd700};
+		z_prev = {10'd300, 10'd400, 10'd500, 10'd600, 10'd700};
+
+        #20 rst = 1;
+        #20 rst = 0;
+
+        #20 start = 1;
+        #2  start = 0;
+
+	end
+      
+endmodule
+
