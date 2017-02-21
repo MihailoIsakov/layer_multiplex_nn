6907e134dad3658ce5a402f3c10729e8ee884e6d
Mihailo Isakov <mihailo@protonmail.com>
2017-02-21 18:37:06 -0500

    large input_aggregator refactor, now requires a start signal for every layer being processed

--- backpropagation/src/forward/input_aggregator.v
+++ backpropagation/src/forward/input_aggregator.v
@@ -32,33 +32,31 @@ module input_aggregator
     input [log2(LAYER_MAX):0]              layer,
     input [NUM_NEURON*INPUT_SIZE-1:0]      layer_input,       // input received from a layer n
     input [NUM_NEURON-1:0]                 layer_input_valid, // validity of layer input
-    output [NUM_NEURON*INPUT_SIZE-1:0]     out_inputs,
-    output [NUM_NEURON-1:0]                active,
-    output                                 layer_start,
-    output reg [NUM_NEURON*INPUT_SIZE-1:0] final_output,
-    output reg                             final_output_valid
+    output [NUM_NEURON*INPUT_SIZE-1:0]     out_inputs,        // sent to the layer module
+    output [NUM_NEURON-1:0]                active,            // sent to the layer module
+    output                                 layer_start,       // sent to the layer module
+    output reg [NUM_NEURON*INPUT_SIZE-1:0] final_output,      // sent to the outside
+    output reg                             final_output_valid // sent to the outside
 );
 
     `include "../log2.v"
     
-    reg [NUM_NEURON-1:0]            active_buffer;
-    reg [NUM_NEURON*INPUT_SIZE-1:0] outputs_buffer;
-    reg [1:0]                       state;
-    reg                             start_out;
+    reg [NUM_NEURON-1:0] active_buffer;
+    reg                  state;
+    reg                  layer_start_buffer;
     // when IA sends out the start signal, the valid input from OA changes after 3 cycles. 
     // This timer prevents IA to fire multiple times before the signal makes a full circle.
     reg [5:0]                       timer; 
 
-    localparam IDLE = 0, WAIT = 1, START = 2;
+    localparam IDLE = 0, WAIT = 1;
     
     always @ (posedge clk) begin
         if (rst) begin
             active_buffer  <= 0;
-            outputs_buffer <= 0; 
             state          <= IDLE;
-            start_out      <= 0;
+            layer_start_buffer      <= 0;
             timer          <= 0;
-            active_buffer  <= {LAYER_MAX{1'b1}};
+            active_buffer  <= {NUM_NEURON{1'b1}};
             //finals
             final_output       <= 0;
             final_output_valid <= 0;
@@ -66,15 +64,8 @@ module input_aggregator
         else begin
             case (state)
                 IDLE: begin
-                    outputs_buffer <= outputs_buffer;
-                    state       <= (start) ? START : IDLE;
-                    start_out   <= 0;
-                end
-
-                START: begin
-                    outputs_buffer     <= (layer == 0) ? start_input : layer_input;
-                    state              <= WAIT;
-                    start_out          <= 1;
+                    state              <= start ? WAIT : IDLE;
+                    layer_start_buffer <= start ? 1     : 0;
                     timer              <= 0;
                     final_output       <= 0;
                     final_output_valid <= 0;
@@ -82,30 +73,26 @@ module input_aggregator
 
                 WAIT: begin
                     timer <= timer + 1;
-                    if ((layer_input_valid & active_buffer) == active_buffer && timer > 10) begin // FIXME
-                        if (layer == LAYER_MAX-1) begin // last layer
-                            state <= IDLE;
-                            final_output       <= layer_input;
-                            final_output_valid <= 1;
-                        end
-                        else begin // not last layer
-                            state <= START;
-                        end
-                        //outputs_buffer <= layer_input;
-                        outputs_buffer <= outputs_buffer;
-                        start_out      <= 0;
+                    if ((layer_input_valid & active_buffer) == active_buffer && timer > 5) begin // FIXME
+                        state              <= IDLE;
+                        layer_start_buffer <= 0;
+                        final_output       <= layer_input;
+                        final_output_valid <= 1;
                     end
                     else begin
-                        outputs_buffer <= outputs_buffer;
-                        state          <= WAIT;
-                        start_out      <= 0;
+                        state              <= WAIT;
+                        layer_start_buffer <= 0;
+                        final_output       <= layer_input;
+                        final_output_valid <= 1;
                     end
                 end
 
                 default: begin
-                    outputs_buffer <= outputs_buffer;
-                    state          <= state;
-                    start_out      <= 0;
+                    state              <= IDLE;
+                    layer_start_buffer <= 0;
+                    timer              <= 0;
+                    final_output       <= 0;
+                    final_output_valid <= 0;
                 end
 
             endcase 
@@ -113,8 +100,8 @@ module input_aggregator
     end
 
     // outputs 
-    assign out_inputs  = outputs_buffer;
-    assign layer_start = start_out;
+    assign out_inputs = (layer == 0) ? start_input : layer_input;
+    assign layer_start = layer_start_buffer;
     assign active      = active_buffer;
 
 
