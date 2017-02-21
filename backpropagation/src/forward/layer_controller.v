961e72a20ea15b2dd8a9bda8106721954731ca45
Mihailo Isakov <mihailo@protonmail.com>
2017-02-21 14:06:27 -0500

    layer controller receives layer number from ouside, passes it to input aggregator

--- backpropagation/src/forward/layer_controller.v
+++ backpropagation/src/forward/layer_controller.v
@@ -30,47 +30,38 @@ module layer_controller
 (
     input clk,
     input rst,
-    input                             start,              // start signal received from the outside
-    input [NUM_NEURON*INPUT_SIZE-1:0] start_input,        // outside input received at the start
-    input [NUM_NEURON*ADDR_SIZE-1:0]  layer_output,       // input from previous layer, in case the layer is > 1
-    input [NUM_NEURON-1:0]            layer_output_valid, //validity of layer's inputs
-    output                                         layer_start,        // start signal sent to neurons
-    output [NUM_NEURON-1:0]                        active,             // activation signal to each neuron
-    output [NUM_NEURON*INPUT_SIZE-1:0]             layer_input,        // inputs sent to all neurons
-    output [NUM_NEURON*INPUT_SIZE-1:0]             final_output,       // final output when the layer counter is LAYER_MAX
-    output                                         final_output_valid  // 1 bit validity of the final output
+    input                              start,              // start signal received from the outside
+    input [NUM_NEURON*INPUT_SIZE-1:0]  start_input,        // outside input received at the start
+    input [log2(LAYER_MAX):0]          layer_number,
+    input [NUM_NEURON*ADDR_SIZE-1:0]   layer_output,       // input from previous layer, in case the layer is > 1
+    input [NUM_NEURON-1:0]             layer_output_valid, //validity of layer's inputs
+    output                             layer_start,        // start signal sent to neurons
+    output [NUM_NEURON-1:0]            active,             // activation signal to each neuron
+    output [NUM_NEURON*INPUT_SIZE-1:0] layer_input,        // inputs sent to all neurons
+    output [NUM_NEURON*INPUT_SIZE-1:0] final_output,       // final output when the layer counter is LAYER_MAX
+    output                             final_output_valid  // 1 bit validity of the final output
 );
 
-    //define the log2 function
-    function integer log2;
-        input integer num;
-        integer i, result;
-        begin
-            for (i = 0; 2 ** i < num; i = i + 1)
-                result = i + 1;
-            log2 = result;
-        end
-    endfunction
+    `include "../log2.v"
 
     wire [NUM_NEURON*INPUT_SIZE-1:0]  OA_output;
     wire [NUM_NEURON-1:0]             OA_output_valid;
-    //wire [log2(LAYER_MAX):0]          layer_num;  // number of the current layer, generated in input_aggregator
 
     input_aggregator #(
         .LAYER_MAX(LAYER_MAX),
         .NUM_NEURON(NUM_NEURON),
-        .INPUT_SIZE(INPUT_SIZE),
+        .INPUT_SIZE(INPUT_SIZE)
     )
     IA (
         .clk(clk), 
         .rst(rst), 
         .start(start), 
         .start_input(start_input), 
+        .layer(layer_number),
         .layer_input(OA_output), 
         .layer_input_valid(OA_output_valid), 
         .out_inputs(layer_input), 
         .active(active), 
-        //.layer_num(layer_num), 
         .layer_start(layer_start),
         .final_output(final_output),
         .final_output_valid(final_output_valid)
