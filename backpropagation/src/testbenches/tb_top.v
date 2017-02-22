4086128e3364a37c797570eaa75bb2157bafaa04
Mihailo Isakov <mihailo@protonmail.com>
2017-02-22 14:12:46 -0500

    tb_top test

---
+++ backpropagation/src/testbenches/tb_top.v
@@ -0,0 +1,85 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:33:12 02/22/2017
+// Design Name:   top
+// Module Name:   /home/mihailo/development/projects/layer_multiplex_nn/backpropagation/tb_top.v
+// Project Name:  backpropagation
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: top
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tb_top;
+
+    parameter NEURON_NUM          = 5,  // number of cells in the vectors a and delta
+              NEURON_OUTPUT_WIDTH = 10, // size of the output of the neuron (z signal)
+              ACTIVATION_WIDTH    = 9,  // size of the neurons activation
+              DELTA_CELL_WIDTH    = 18, // width of each delta cell
+              WEIGHT_CELL_WIDTH   = 16, // width of individual weights
+              FRACTION_WIDTH      = 8,
+              LEARNING_RATE_SHIFT = 0,
+              LAYER_ADDR_WIDTH    = 2,
+              LAYER_MAX           = 3,  // number of layers in the network
+              SAMPLE_ADDR_SIZE    = 10, // size of the sample addresses
+              MAX_SAMPLES         = 10000,
+              TARGET_FILE         = "targets.list",
+              WEIGHT_INIT_FILE    = "weight_init.list";
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg start;
+
+	// Instantiate the Unit Under Test (UUT)
+    top #(
+        .NEURON_NUM         (NEURON_NUM         ),
+        .NEURON_OUTPUT_WIDTH(NEURON_OUTPUT_WIDTH),
+        .ACTIVATION_WIDTH   (ACTIVATION_WIDTH   ),
+        .DELTA_CELL_WIDTH   (DELTA_CELL_WIDTH   ),
+        .WEIGHT_CELL_WIDTH  (WEIGHT_CELL_WIDTH  ),
+        .FRACTION_WIDTH     (FRACTION_WIDTH     ),
+        .LEARNING_RATE_SHIFT(LEARNING_RATE_SHIFT),
+        .LAYER_ADDR_WIDTH   (LAYER_ADDR_WIDTH   ),
+        .LAYER_MAX          (LAYER_MAX          ),
+        .SAMPLE_ADDR_SIZE   (SAMPLE_ADDR_SIZE   ),
+        .MAX_SAMPLES        (MAX_SAMPLES        ),
+        .TARGET_FILE        (TARGET_FILE        ),
+        .WEIGHT_INIT_FILE   (WEIGHT_INIT_FILE   )
+    ) uut (
+		.clk(clk), 
+		.rst(rst), 
+		.start(start)
+	);
+
+    always 
+        #1 clk = ~clk;
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		start = 0;
+
+        #20 rst = 1;
+        #2  rst = 0;
+
+        #20 start = 1;
+        #2  start = 0;
+
+	end
+      
+endmodule
+
