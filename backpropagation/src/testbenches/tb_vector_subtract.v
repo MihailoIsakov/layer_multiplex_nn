c06418e7d982f43ab25bb31fda63b9a713fc8693
Mihailo Isakov <mihailo@protonmail.com>
2017-03-08 21:52:28 -0500

    refactored vector_subtract

--- backpropagation/src/testbenches/tb_vector_subtract.v
+++ backpropagation/src/testbenches/tb_vector_subtract.v
@@ -23,6 +23,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 module tb_vector_subtract;
+
     parameter VECTOR_LEN        = 5,
               A_CELL_WIDTH      = 8,
               B_CELL_WIDTH      = 8,
@@ -32,13 +33,17 @@ module tb_vector_subtract;
 	// Inputs
 	reg clk;
 	reg rst;
-	reg start;
     reg [VECTOR_LEN*A_CELL_WIDTH-1:0] a;
+    reg                               a_valid;
     reg [VECTOR_LEN*B_CELL_WIDTH-1:0] b;
+    reg                               b_valid;
+    reg                               result_ready;
 
 	// Outputs
 	wire [VECTOR_LEN*RESULT_CELL_WIDTH-1:0] result;
-	wire valid;
+    wire                                    result_valid;
+    wire                                    a_ready;
+    wire                                    b_ready;
     wire error;
 
     // memory
@@ -51,7 +56,7 @@ module tb_vector_subtract;
     endgenerate
 
 	// Instantiate the Unit Under Test (UUT)
-	vector_subtract
+	vector_add 
     #(
         .VECTOR_LEN       (VECTOR_LEN       ),
         .A_CELL_WIDTH     (A_CELL_WIDTH     ),
@@ -61,35 +66,49 @@ module tb_vector_subtract;
     uut (
 		.clk(clk), 
 		.rst(rst), 
-		.start(start), 
 		.a(a), 
+        .a_valid(a_valid),
+        .a_ready(a_ready),
 		.b(b), 
+        .b_valid(b_valid),
+        .b_ready(b_ready),
 		.result(result), 
-		.valid(valid),
+        .result_valid(result_valid),
+        .result_ready(result_ready),
         .error(error)
 	);
 
     always
-        #1 clk = ~clk;
+        #1 clk <= ~clk;
 
 	initial begin
 		// Initialize Inputs
-		clk = 0;
-		rst = 0;
-		start = 0;
-		a = {-8'd50, 8'd40, 8'd30, 8'd20, -8'd10}; // 10, 20, 30, 40, 50
-		b = {8'd8  , 8'd2 , -8'd3, 8'd25 , 8'd5};  // 5  , 4 , 3 , 2 , 1
+		clk <= 0;
+		rst <= 1;
 
-        #20 rst = 1;
-        #20 rst = 0;
+		a <= {-8'd50, 8'd40, 8'd30, 8'd20, -8'd10}; // 10, 20, 30, 40, 50
+        a_valid <= 0;
+		b <= {8'd1  , 8'd2 , -8'd3, 8'd4 , 8'd5};  // 5  , 4 , 3 , 2 , 1
+        b_valid <= 0;
+        result_ready <= 0;
 
-        #20 start = 1;
-        #2  start = 0;
+        #10 rst <= 0;
+    
+        #20 a_valid <= 1;
+        #10 b_valid <= 1;
+        #5  a_valid <= 0;
+        #5  b_valid <= 0;
 
-        #40 start = 1;
-            a = {-8'd50, 8'd30, 8'd20, 8'd127, -8'd10}; // 10, 20, 30, 40, 50
-            b = {-8'd47, -8'd3, 8'd4 , -8'd50, 8'd5};  // 5  , 4 , 3 , 2 , 1
-        #2  start = 0;
+        #20 result_ready <= 1;
+
+        #20
+            a = {8'd127, -8'd128, 8'd30, 8'd20, -8'd10}; // 10, 20, 30, 40, 50
+            b = {8'd127, -8'd128, -8'd3, 8'd4 , 8'd5};  // 5  , 4 , 3 , 2 , 1
+        #9  a_valid <= 1;
+            b_valid <= 1;
+            result_ready <= 1;
+
+
 
 	end
       
