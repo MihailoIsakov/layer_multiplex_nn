62373da4a0fb2e613fbc5bd54b51c8887e5761ae
Mihailo Isakov <mihailo@protonmail.com>
2017-03-14 21:51:59 -0400

    updated testbenches

--- backpropagation/src/testbenches/tb_error_propagator.v
+++ backpropagation/src/testbenches/tb_error_propagator.v
@@ -30,30 +30,41 @@ module tb_error_propagator;
               WEIGHTS_CELL_WIDTH = 8,  // widht of each matrix cell in bits
               NEURON_ADDR_WIDTH  = 10, // width of activations from neurons before the sigmoid
               ACTIVATION_WIDTH   = 9,  // cell width after sigmoid
-              FRACTION_WIDTH     = 0,
+              FRACTION_WIDTH     = 1,
               TILING_ROW         = 3,  // number of vector_mac units to create
               TILING_COL         = 3;  // number of multipliers per vector_mac unit
 
-    `include "log2.v"
-
-    localparam DW = DELTA_CELL_WIDTH;
 
 	// Inputs
 	reg clk;
 	reg rst;
-	reg                                                     start;
-	reg [MATRIX_HEIGHT*DELTA_CELL_WIDTH-1:0]                delta_input;
-	reg [MATRIX_WIDTH *NEURON_ADDR_WIDTH-1:0]               z;
-	reg [MATRIX_WIDTH*MATRIX_HEIGHT*WEIGHTS_CELL_WIDTH-1:0] w;
 
-	// Outputs
-	wire [MATRIX_WIDTH*DELTA_CELL_WIDTH-1:0] delta_output;
-	wire valid, error;
+    // delta input
+	reg [MATRIX_WIDTH*DELTA_CELL_WIDTH-1:0]                 delta_input;
+    reg                                                     delta_input_valid;
+    wire                                                    delta_input_ready;
+    
+    // z
+	reg [MATRIX_HEIGHT*NEURON_ADDR_WIDTH-1:0]               z;
+    reg                                                     z_valid;
+    wire                                                    z_ready;
+
+    // w
+	reg [MATRIX_WIDTH*MATRIX_HEIGHT*WEIGHTS_CELL_WIDTH-1:0] w;
+    reg                                                     w_valid;
+    wire                                                    w_ready;
+
+	// delta output
+	wire [MATRIX_HEIGHT*DELTA_CELL_WIDTH-1:0]                delta_output;
+    wire                                                    delta_output_valid;
+    reg                                                     delta_output_ready;
+
+	wire error;
 
     // Memory
-    wire [DELTA_CELL_WIDTH-1:0] delta_output_mem [MATRIX_WIDTH-1:0];
+    wire [DELTA_CELL_WIDTH-1:0] delta_output_mem [MATRIX_HEIGHT-1:0];
     genvar i;
-    for (i=0; i<MATRIX_WIDTH; i=i+1) begin: MEM
+    for (i=0; i<MATRIX_HEIGHT; i=i+1) begin: MEM
         assign delta_output_mem[i] = delta_output[i*DELTA_CELL_WIDTH+:DELTA_CELL_WIDTH];
     end
 
@@ -72,33 +83,45 @@ module tb_error_propagator;
     ) uut (
 		.clk         (clk         ),
 		.rst         (rst         ),
-		.start       (start       ),
-		.delta_input (delta_input ),
-		.z           (z           ),
-		.w           (w           ),
-		.delta_output(delta_output),
-		.valid       (valid       ),
-        .error       (error       )
+        .delta_input       (delta_input       ),
+        .delta_input_valid (delta_input_valid ),
+        .delta_input_ready (delta_input_ready ),
+        .z                 (z                 ),
+        .z_valid           (z_valid           ),
+        .z_ready           (z_ready           ),
+        .w                 (w                 ),
+        .w_valid           (w_valid           ),
+        .w_ready           (w_ready           ),
+        .delta_output      (delta_output      ),
+        .delta_output_valid(delta_output_valid),
+        .delta_output_ready(delta_output_ready),
+        .error             (error             )
 	);
 
     always 
-        #1 clk = ~clk;
+        #1 clk <= ~clk;
 
 	initial begin
 		// Initialize Inputs
-		clk = 0;
-		rst = 0;
-		start = 0;
-		delta_input = {12'd1, 12'd1, 12'd1, 12'd1, 12'd1}; // 10, 20, 30, 40, 50
-		z = {10'd250, 10'd220, 10'd190, 10'd120};  // 4,  3,  2,  1
-        w = {8'd20, 8'd19, 8'd18, 8'd17, 8'd16, 8'd15, 8'd14, 8'd13, 8'd12, 8'd11, 8'd10, 8'd9, 8'd8, 8'd7, 8'd6, 8'd5, 8'd4, 8'd3, 8'd2, 8'd1};
+		clk                    <= 0;
+		rst                    <= 1;
+		delta_input            <= {12'd1, 12'd1, 12'd1, 12'd1}; // 10, 20, 30, 40, 50
+        delta_input_valid      <= 0;
+		z                      <= {10'd270, 10'd250, 10'd230, 10'd220, 10'd200};  // 4,  3,  2,  1
+        z_valid                <= 0;
+        w                      <= {8'd20, 8'd19, 8'd18, 8'd17, 8'd16, 8'd15, 8'd14, 8'd13, 8'd12, 8'd11, 8'd10, 8'd9, 8'd8, 8'd7, 8'd6, 8'd5, 8'd4, 8'd3, 8'd2, 8'd1};
+        w_valid                <= 0;
         // [  45.,  100.,  165.,  240.]
-        
-        #20 rst = 1;
-        #20 rst = 0;
+        delta_output_ready     <= 0;
 
-        #20 start = 1;
-        #2  start = 0;
+        #20 rst                <= 0;
+
+        #10 delta_input_valid  <= 1;
+        #10 z_valid            <= 1;
+        #10 w_valid            <= 1;
+
+        #40 delta_output_ready <= 1;
+
 
 	end
       
--- backpropagation/src/testbenches/tb_mvm.v
+++ backpropagation/src/testbenches/tb_mvm.v
@@ -24,16 +24,7 @@
 
 module tb_mvm;
 
-    //define the log2 function
-    function integer log2;
-        input integer num;
-        integer i, result;
-        begin
-            for (i = 0; 2 ** i < num; i = i + 1)
-                result = i + 1;
-            log2 = result;
-        end
-    endfunction
+    `include "log2.v"
 
     parameter MATRIX_WIDTH      = 20, // width of the matrix aka the number of columns
               MATRIX_HEIGHT     = 5, // height of the matrix aka the number of rows and size of vector
@@ -41,21 +32,29 @@ module tb_mvm;
               MATRIX_CELL_WIDTH = 8, // widht of each matrix cell in bits
               RESULT_CELL_WIDTH = 12, // width of each result cell in bits
               FRACTION_WIDTH    = 4,
-              TILING_ROW        = 2, // number of vector_mac units to create
-              TILING_COL        = 20;  // number of multipliers per vector_mac unit
+              TILING_ROW        = 3, // number of vector_mac units to create
+              TILING_COL        = 2;  // number of multipliers per vector_mac unit
 
     localparam VECTOR_WIDTH = MATRIX_HEIGHT;
 
-	// Inputs
 	reg clk;
 	reg rst;
-	reg start;
-	reg [MATRIX_HEIGHT*VECTOR_CELL_WIDTH-1:0] vector;
-	reg [MATRIX_WIDTH*MATRIX_HEIGHT*MATRIX_CELL_WIDTH-1:0] matrix;
 
-	// Outputs
+    // vector signals
+	reg [MATRIX_HEIGHT*VECTOR_CELL_WIDTH-1:0] vector;
+    reg                                       vector_valid;
+    wire                                      vector_ready;
+
+    // matrix signals
+	reg [MATRIX_WIDTH*MATRIX_HEIGHT*MATRIX_CELL_WIDTH-1:0] matrix;
+    reg                                                    matrix_valid;
+    wire                                                   matrix_ready;
+
+    // result signals
 	wire [MATRIX_WIDTH*RESULT_CELL_WIDTH-1:0] result;
-	wire valid, error;
+    wire                                      result_valid;
+    reg                                       result_ready;
+    wire                                      error;
 
     // Memories
     wire [RESULT_CELL_WIDTH-1:0] result_mem [0:MATRIX_WIDTH-1];
@@ -66,47 +65,62 @@ module tb_mvm;
     end
     endgenerate
 
-	// Instantiate the Unit Under Test (UUT)
-	mvm 
-    #(
-        .MATRIX_WIDTH     (MATRIX_WIDTH     ),
-        .MATRIX_HEIGHT    (MATRIX_HEIGHT    ),
-        .VECTOR_CELL_WIDTH(VECTOR_CELL_WIDTH),
-        .MATRIX_CELL_WIDTH(MATRIX_CELL_WIDTH),
-        .RESULT_CELL_WIDTH(RESULT_CELL_WIDTH),
-        .FRACTION_WIDTH   (FRACTION_WIDTH   ),
-        .TILING_ROW       (TILING_ROW       ),
-        .TILING_COL       (TILING_COL       )
-    ) uut (
-		.clk(clk), 
-		.rst(rst), 
-		.start(start), 
-		.vector(vector), 
-		.matrix(matrix), 
-		.result(result), 
-		.valid(valid),
-        .error(error)
-	);
+
+    mvm #(
+    .MATRIX_WIDTH     (MATRIX_WIDTH     ),
+    .MATRIX_HEIGHT    (MATRIX_HEIGHT    ),
+    .VECTOR_CELL_WIDTH(VECTOR_CELL_WIDTH),
+    .MATRIX_CELL_WIDTH(MATRIX_CELL_WIDTH),
+    .RESULT_CELL_WIDTH(RESULT_CELL_WIDTH),
+    .FRACTION_WIDTH   (FRACTION_WIDTH   ),
+    .TILING_ROW       (TILING_ROW       ),
+    .TILING_COL       (TILING_COL       )
+    ) mvm (
+        .clk         (clk         ),
+        .rst         (rst         ),
+        .vector      (vector      ),
+        .vector_valid(vector_valid),
+        .vector_ready(vector_ready),
+        .matrix      (matrix      ),
+        .matrix_valid(matrix_valid),
+        .matrix_ready(matrix_ready),
+        .result      (result      ),
+        .result_valid(result_valid),
+        .result_ready(result_ready),
+        .error       (error       )
+    );
 
     always 
-        #1 clk = ~clk;
+        #1 clk <= ~clk;
 
 	initial begin
 		// Initialize Inputs
-		clk = 0;
-		rst = 0;
-		start = 0;
-		vector = {8'd5, 8'd4, 8'd3, 8'd2, 8'd1};
-		matrix = {8'd101,  8'd100,  8'd99,  8'd98,  8'd97,  8'd96,  8'd95,  8'd94,  8'd93,  8'd92,  8'd91,  8'd90,  8'd89,  8'd88,  8'd87,  8'd86,  8'd85,  8'd84,  8'd83,  8'd82,  8'd81,  8'd80,  8'd79,  8'd78,  8'd77,  8'd76,  8'd75,  8'd74,  8'd73,  8'd72,  8'd71,  8'd70,  8'd69,  8'd68,  8'd67,  8'd66,  8'd65,  8'd64,  8'd63,  8'd62,  8'd61,  8'd60,  8'd59,  8'd58,  8'd57,  8'd56,  8'd55,  8'd54,  8'd53,  8'd52,  8'd51,  8'd50,  8'd49,  8'd48,  8'd47,  8'd46,  8'd45,  8'd44,  8'd43,  8'd42,  8'd41,  8'd40,  8'd39,  8'd38,  8'd37,  8'd36,  8'd35,  8'd34,  8'd33,  8'd32,  8'd31,  8'd30,  8'd29,  8'd28,  8'd27,  8'd26,  8'd25,  8'd24,  8'd23,  8'd22,  8'd21,  8'd20,  8'd19,  8'd18,  8'd17,  8'd16,  8'd15,  8'd14,  8'd13,  8'd12,  8'd11,  8'd10,  8'd9,  8'd8,  8'd7,  8'd6,  8'd5,  8'd4,  8'd3,  8'd2, 8'd1};
-        // expected result = [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 65, 66, 67, 68]
+		clk <= 0;
+		rst <= 1;
+        vector_valid <= 0;
+        matrix_valid <= 0;
+        result_ready <= 0;
+
+		vector <= {8'd5, 8'd4, 8'd3, 8'd2, 8'd1};
+        vector_valid <= 0;
+
+		matrix <= {8'd101,  8'd100,  8'd99,  8'd98,  8'd97,  8'd96,  8'd95,  8'd94,  8'd93,  8'd92,  8'd91,  8'd90,  8'd89,  8'd88,  8'd87,  8'd86,  8'd85,  8'd84,  8'd83,  8'd82,  8'd81,  8'd80,  8'd79,  8'd78,  8'd77,  8'd76,  8'd75,  8'd74,  8'd73,  8'd72,  8'd71,  8'd70,  8'd69,  8'd68,  8'd67,  8'd66,  8'd65,  8'd64,  8'd63,  8'd62,  8'd61,  8'd60,  8'd59,  8'd58,  8'd57,  8'd56,  8'd55,  8'd54,  8'd53,  8'd52,  8'd51,  8'd50,  8'd49,  8'd48,  8'd47,  8'd46,  8'd45,  8'd44,  8'd43,  8'd42,  8'd41,  8'd40,  8'd39,  8'd38,  8'd37,  8'd36,  8'd35,  8'd34,  8'd33,  8'd32,  8'd31,  8'd30,  8'd29,  8'd28,  8'd27,  8'd26,  8'd25,  8'd24,  8'd23,  8'd22,  8'd21,  8'd20,  8'd19,  8'd18,  8'd17,  8'd16,  8'd15,  8'd14,  8'd13,  8'd12,  8'd11,  8'd10,  8'd9,  8'd8,  8'd7,  8'd6,  8'd5,  8'd4,  8'd3,  8'd2, 8'd1};
+        matrix_valid <= 0;
+        // expected result <= [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 65, 66, 67, 68]
 
         
-        #20 rst = 1;
-        #2  rst = 0;
+        #20 rst <= 0;
 
-        #20 start = 1;
-        #2  start = 0;
+        #20 vector_valid <= 1;
+        #10 vector_valid <= 0;
+        #20 matrix_valid <= 1;
+        #10 matrix_valid <= 0;
 
+        #100 result_ready <= 1;
+        
+        #100 vector_valid <= 1;
+             matrix_valid <= 1;
+             result_ready <= 1;
 
 	end
       
