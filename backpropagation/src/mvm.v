c996f429737fbbdc00007400ed239744e36eaf65
Mihailo Isakov <mihailo@protonmail.com>
2017-03-14 21:51:27 -0400

    supports fixed oints

--- backpropagation/src/mvm.v
+++ backpropagation/src/mvm.v
@@ -89,7 +89,7 @@ module mvm
     reg [log2(MATRIX_HEIGHT):0] counter_h; // goes down the matrix columns, goes up to TILING_ROW
     reg [log2(MATRIX_WIDTH):0]  counter_w; // goes across columns, goes up to TILING_COL
 
-    genvar h, w;
+    genvar h, w, i;
 
     generate 
     always @ (posedge clk) begin
@@ -99,33 +99,31 @@ module mvm
             result_buffer = 0;
             error_buffer  = 0;
         end
-        else if (state == CALC) begin
-            case(state)
-                IDLE: begin
-                    counter_h     <= 0;
-                    counter_w     <= 0;
-                    result_buffer = 0;
-                    error_buffer  = 0;
-                end
-                CALC: begin
-                    counter_h <= (counter_h + TILING_ROW < MATRIX_HEIGHT) ? counter_h + TILING_ROW : 0;
-                    counter_w <= (counter_h + TILING_ROW < MATRIX_HEIGHT) ? counter_w : counter_w + TILING_COL;
-                    error_buffer  = 0;
-                end
-                DONE: begin
-                    counter_h     <= 0;
-                    counter_w     <= 0;
-                    result_buffer = result_buffer;
-                    error_buffer  = 0;
-                end
-                default: begin
-                    counter_h     <= 0;
-                    counter_w     <= 0;
-                    result_buffer = 0;
-                    error_buffer  = 0;
-                end
-            endcase
-        end
+        case(state)
+            IDLE: begin
+                counter_h     <= 0;
+                counter_w     <= 0;
+                result_buffer = 0;
+                error_buffer  = 0;
+            end
+            CALC: begin
+                counter_h     <= (counter_h + TILING_ROW < MATRIX_HEIGHT) ? counter_h + TILING_ROW : 0;
+                counter_w     <= (counter_h + TILING_ROW < MATRIX_HEIGHT) ? counter_w : counter_w + TILING_COL;
+                error_buffer  = 0;
+            end
+            DONE: begin
+                counter_h     <= 0;
+                counter_w     <= 0;
+                result_buffer = result_buffer;
+                error_buffer  = 0;
+            end
+            default: begin
+                counter_h     <= 0;
+                counter_w     <= 0;
+                result_buffer = 0;
+                error_buffer  = 0;
+            end
+        endcase
     end
     endgenerate
 
@@ -134,24 +132,36 @@ module mvm
     for (w=0; w<TILING_COL; w=w+1) begin: LEFTRIGHT
         for (h=0; h<TILING_ROW; h=h+1) begin: UPDOWN
             always @ (posedge clk) begin
-                if (state == CALC) begin
-                    result_buffer[(counter_w+w)*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH] = 
-                        result_buffer[(counter_w+w)*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH] + // old result 
-                        ((counter_h+h < MATRIX_HEIGHT) 
-                            ? vector_buffer[(counter_h+h)*VECTOR_CELL_WIDTH+:VECTOR_CELL_WIDTH] 
-                            : 0
-                        ) *
-                        ((counter_w+w < MATRIX_WIDTH && counter_h+h < MATRIX_HEIGHT)  
-                            ? matrix_buffer[((counter_h+h)*MATRIX_WIDTH+counter_w+w)*MATRIX_CELL_WIDTH+:MATRIX_CELL_WIDTH] 
-                            : 0
-                        );
-                end
+                case (state)
+                    IDLE:
+                        result_buffer = 0;
+                    CALC: begin
+                        result_buffer[(counter_w+w)*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH] = 
+                            result_buffer[(counter_w+w)*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH] + // old result 
+                            ((counter_h+h < MATRIX_HEIGHT) 
+                                ? vector_buffer[(counter_h+h)*VECTOR_CELL_WIDTH+:VECTOR_CELL_WIDTH] 
+                                : 0
+                            ) *
+                            ((counter_w+w < MATRIX_WIDTH && counter_h+h < MATRIX_HEIGHT)  
+                                ? matrix_buffer[((counter_h+h)*MATRIX_WIDTH+counter_w+w)*MATRIX_CELL_WIDTH+:MATRIX_CELL_WIDTH] 
+                                : 0
+                            );
+                    end
+                    DONE:
+                        result_buffer = result_ready ? 0 : result_buffer;
+                endcase
             end
         end
     end
     endgenerate
 
-    assign result       = result_buffer;
+    // shift by FRACTION_WIDTH
+    generate 
+    for (i=0; i<MATRIX_WIDTH; i=i+1) begin: SHIFT
+        assign result[i*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH] = result_buffer[i*RESULT_CELL_WIDTH+:RESULT_CELL_WIDTH] >> FRACTION_WIDTH;
+    end
+    endgenerate
+    //assign result       = result_buffer;
     assign result_valid = state == DONE;
     assign vector_ready = !vector_set;
     assign matrix_ready = !matrix_set;
