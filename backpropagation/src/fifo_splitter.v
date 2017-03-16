338583c357de918d17ad63166dc4738b92df2716
Mihailo Isakov <mihailo@protonmail.com>
2017-03-16 13:48:22 -0400

    prettier fifo splitter

--- backpropagation/src/fifo_splitter.v
+++ backpropagation/src/fifo_splitter.v
@@ -18,23 +18,23 @@ module fifo_splitter #(
 );
 
     reg [DATA_WIDTH-1:0] data_buffer;
-    reg used1, used2;
+    reg valid1, valid2;
 
     always @ (posedge clk) begin
         if (rst) begin
             data_buffer <= 0;
-            used1       <= 1;
-            used2       <= 1;
+            valid1      <= 0;
+            valid2      <= 0;
         end
         else begin
-            if (used1 && used2 && data_in_valid) begin // ask for next data
+            if (~valid1 && ~valid2 && data_in_valid) begin // ask for next data
                 data_buffer <= data_in;
-                used1       <= 0;
-                used2       <= 0; 
+                valid1      <= 1;
+                valid2      <= 1; 
             end
             else begin
-                used1 <= (~used1 && data_out1_ready) ? 1 : used1;
-                used2 <= (~used2 && data_out2_ready) ? 1 : used2;
+                valid1 <= (valid1 && data_out1_ready) ? 0 : valid1;
+                valid2 <= (valid2 && data_out2_ready) ? 0 : valid2;
             end
         end
     end
@@ -42,8 +42,8 @@ module fifo_splitter #(
     // outputs
     assign data_out1       = data_buffer;
     assign data_out2       = data_buffer;
-    assign data_in_ready   = (used1 && used2);
-    assign data_out1_valid = ~used1;
-    assign data_out2_valid = ~used2;
+    assign data_in_ready   = (~valid1 && ~valid2);
+    assign data_out1_valid = valid1;
+    assign data_out2_valid = valid2;
 
 endmodule
