870c3cd13c104a0156a60dcca7305bfc532e0984
Mihailo Isakov <mihailo@protonmail.com>
2017-06-19 13:36:47 -0400

    rewrote neuron with the valid/ready protocol

---
+++ backpropagation/src/neuron.v
@@ -0,0 +1,120 @@
+module neuron #(
+    parameter NEURON_NUM = 5,
+              NEURON_OUTPUT_WIDTH = 10,
+              WEIGHT_CELL_WIDTH   = 16
+) (
+    input clk,
+    input rst,
+    input [log2(NEURON_NUM):0]                 input_number, // number of neurons from previous layer
+    input                                      input_number_valid,
+    output                                     input_number_ready,
+    input [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0] inputs,       // values of these inputs
+    input                                      inputs_valid,
+    output                                     inputs_ready,
+    input [NEURON_NUM*WEIGHT_CELL_WIDTH-1  :0] weights,      // values of corresponding weights
+    input                                      weights_valid,
+    output                                     weights_ready,
+    output [NEURON_OUTPUT_WIDTH-1:0]           neuron_sum,   // sum of inputs multiplied by values
+    output                                     overflow,     // overflow/underflow flag, paired with neuron sum, read at same time
+    output                                     neuron_sum_valid,
+    input                                      neuron_sum_ready
+);
+
+    `include "log2.v"
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Registers
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    reg [log2(NEURON_NUM)                :0] input_number_buffer;
+    reg                                      input_number_set;
+    reg [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0] inputs_buffer;
+    reg                                      inputs_set;
+    reg [NEURON_NUM*WEIGHT_CELL_WIDTH  -1:0] weights_buffer;
+    reg                                      weights_set;
+
+    reg signed [NEURON_OUTPUT_WIDTH-1:0] sum; // current sum of input-weight products
+    wire signed [NEURON_OUTPUT_WIDTH-1:0] product;
+    reg overflow_buffer;                      // flag indicating an overflow/underflow happened during summing
+    wire extra, of, uf;                       // wires used for determining overflow/underflow
+
+    assign of = ({extra, sum[NEURON_OUTPUT_WIDTH-1]} == 2'b01);
+    assign uf = ({extra, sum[NEURON_OUTPUT_WIDTH-1]} == 2'b10);
+    assign {extra, product} = $signed(inputs_buffer[counter*NEURON_OUTPUT_WIDTH+:NEURON_OUTPUT_WIDTH]) * $signed(weights_buffer[counter*WEIGHT_CELL_WIDTH+:WEIGHT_CELL_WIDTH]);
+
+    reg [log2(NEURON_NUM):0] counter;         // current input connection being processed (counter < NEURON_NUM)
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // State machine
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    localparam IDLE=0, CALC=1, DONE=2;
+    reg [1:0] state;
+
+    always @ (posedge clk) begin
+        if (rst) begin
+            state               <= IDLE;
+            input_number_buffer <= 0;
+            input_number_set    <= 0;
+            inputs_buffer       <= 0;
+            inputs_set          <= 0;
+            weights_buffer      <= 0;
+            weights_set         <= 0;
+            sum                 <= 0;
+            overflow_buffer     <= 0;
+            counter             <= 0;
+        end
+        else begin
+            case (state) 
+                IDLE: begin
+                    state               <= (input_number_set && inputs_set && weights_set) ? CALC : IDLE;
+                    input_number_buffer <= (!input_number_set && input_number_valid) ? input_number : input_number_buffer;
+                    input_number_set    <= input_number_set || input_number_valid;
+                    inputs_buffer       <= (!inputs_set && inputs_valid) ? inputs : inputs_buffer;
+                    inputs_set          <= inputs_set || inputs_valid;
+                    weights_buffer      <= (!weights_set && weights_valid) ? weights : weights_buffer;
+                    weights_set         <= weights_set || weights_valid;
+                    sum                 <= 0;
+                    overflow_buffer     <= 0;
+                    counter             <= 0;
+                end
+                CALC: begin
+                    state               <= (counter == (input_number_buffer - 1)) ? DONE : CALC;
+                    input_number_buffer <= input_number_buffer;
+                    input_number_set    <= input_number_set;
+                    inputs_buffer       <= inputs_buffer;
+                    inputs_set          <= inputs_set;
+                    weights_buffer      <= weights_buffer;
+                    weights_set         <= weights_set;             
+                    sum                 <= sum + product;
+                    overflow_buffer     <= overflow_buffer || of || uf;
+                    counter             <= (counter >= (input_number_buffer - 1)) ? 0    : counter + 1;
+                end
+                DONE: begin
+                    state               <= neuron_sum_ready ? IDLE : DONE;
+                    input_number_buffer <= 0;
+                    input_number_set    <= 0;
+                    inputs_buffer       <= 0; 
+                    inputs_set          <= 0; 
+                    weights_buffer      <= 0; 
+                    weights_set         <= 0; 
+                    sum                 <= neuron_sum_ready ? 0    : sum;
+                    overflow_buffer     <= neuron_sum_ready ? 0    : overflow;
+                    counter             <= 0;
+                end
+            endcase
+        end
+    end
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Outputs
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    assign input_number_ready = !input_number_set;
+    assign inputs_ready       = !inputs_set; 
+    assign weights_ready      = !weights_set;
+    assign neuron_sum         = sum; 
+    assign overflow           = overflow_buffer;
+    assign neuron_sum_valid   = state == DONE;
+
+endmodule
---
+++ backpropagation/src/testbenches/tb_neuron.v
@@ -0,0 +1,102 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/19/2017 12:17:01 PM
+// Design Name: 
+// Module Name: tb_neuron
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_neuron;
+    
+    parameter NEURON_NUM = 5,
+              NEURON_OUTPUT_WIDTH = 10,
+              WEIGHT_CELL_WIDTH   = 16;
+
+    `include "log2.v"
+
+    reg clk;
+    reg rst;
+    reg   [log2(NEURON_NUM):0]                 input_number; 
+    reg                                        input_number_valid;
+    wire                                       input_number_ready;
+    reg   [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0] inputs;    
+    reg                                        inputs_valid;
+    wire                                       inputs_ready;
+    reg   [NEURON_NUM*WEIGHT_CELL_WIDTH-1  :0] weights;    
+    reg                                        weights_valid;
+    wire                                       weights_ready;
+    wire   [NEURON_OUTPUT_WIDTH-1:0]           neuron_sum;  
+    wire                                       overflow;     
+    wire                                       neuron_sum_valid;
+    reg                                        neuron_sum_ready;
+
+    neuron #(
+        .NEURON_NUM(NEURON_NUM),
+        .NEURON_OUTPUT_WIDTH(NEURON_OUTPUT_WIDTH),
+        .WEIGHT_CELL_WIDTH(WEIGHT_CELL_WIDTH)
+    ) neuron (
+        .clk               (clk               ),
+        .rst               (rst               ),
+        .input_number      (input_number      ),
+        .input_number_valid(input_number_valid),
+        .input_number_ready(input_number_ready),
+        .inputs            (inputs            ),
+        .inputs_valid      (inputs_valid      ),
+        .inputs_ready      (inputs_ready      ),
+        .weights           (weights           ),
+        .weights_valid     (weights_valid     ),
+        .weights_ready     (weights_ready     ),
+        .neuron_sum        (neuron_sum        ),
+        .overflow          (overflow          ),
+        .neuron_sum_valid  (neuron_sum_valid  ),
+        .neuron_sum_ready  (neuron_sum_ready  )
+    );
+
+    always 
+        #1 clk = ~clk;
+
+    initial begin
+        clk                    <= 0;
+        rst                    <= 1;
+
+        input_number           <= NEURON_NUM;
+        input_number_valid     <= 1;
+
+        inputs                 <= {10'd5, 10'd4, 10'd3, 10'd2, 10'd1};
+        inputs_valid           <= 1;
+
+        weights                <= {16'd10, 16'd8, 16'd6, 16'd4, 16'd2};
+        weights_valid          <= 1;
+
+        neuron_sum_ready       <= 0;
+
+        #10 rst                <= 0;
+
+        #4 inputs_valid        <= 0;
+           weights_valid       <= 0;
+           input_number_valid  <= 0;
+
+        #20 neuron_sum_ready   <= 1;
+
+        #10 inputs_valid       <= 1;
+            weights_valid      <= 1;
+            input_number_valid <= 1;
+            weights            <= {16'd100, 16'd80, 16'd60, 16'd40, 16'd20};
+
+    end
+
+endmodule
