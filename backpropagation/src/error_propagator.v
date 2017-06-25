1deb6ba50e2e33b5689c0093b9ef5ea7c502e654
Mihailo Isakov <mihailo@protonmail.com>
2017-06-25 15:17:58 -0400

    untested error_propagator edited so that it doesn't take into account the layer

--- backpropagation/src/error_propagator.v
+++ backpropagation/src/error_propagator.v
@@ -13,10 +13,6 @@ module error_propagator
 )(
     input clk,
     input rst,
-    // layer input 
-    input  [LAYER_ADDR_WIDTH-1:0]                              layer,
-    input                                                      layer_valid,
-    output                                                     layer_ready,
     // delta input 
     input  [MATRIX_WIDTH*DELTA_CELL_WIDTH-1:0]                 delta_input,
     input                                                      delta_input_valid,
@@ -37,10 +33,6 @@ module error_propagator
     output                                                     error
 );
     
-    // layer buffer
-    reg [LAYER_ADDR_WIDTH-1:0] layer_buffer;
-    reg layer_set;
-
     wire [MATRIX_HEIGHT*MATRIX_WIDTH*WEIGHTS_CELL_WIDTH-1:0] w_transposed;
 
     wire [MATRIX_HEIGHT*DELTA_CELL_WIDTH-1:0] mvm_result;
@@ -48,9 +40,6 @@ module error_propagator
     wire [MATRIX_HEIGHT*ACTIVATION_WIDTH-1:0] lut_result;
     wire lut_result_ready, lut_result_valid;
     wire mvm_error, dot_error;
-    // dot product results
-    wire [MATRIX_HEIGHT*DELTA_CELL_WIDTH-1:0] dot_result;
-    wire dot_result_valid, dot_result_ready;
 
     transpose #(
         .WIDTH        (MATRIX_WIDTH      ),
@@ -70,7 +59,7 @@ module error_propagator
         .FRACTION_WIDTH   (FRACTION_WIDTH    ),
         .TILING_ROW       (TILING_ROW        ),
         .TILING_COL       (TILING_COL        )) 
-    mvm(
+    mvm (
         .clk         (clk              ),
         .rst         (rst              ),
         .vector      (delta_input      ),
@@ -110,78 +99,43 @@ module error_propagator
         .FRACTION_WIDTH   (FRACTION_WIDTH  ),
         .TILING           (2               )
     ) dot (
-        .clk         (clk             ),
-        .rst         (rst             ),
-        .a           (mvm_result      ),
-        .a_valid     (mvm_result_valid),
-        .a_ready     (mvm_result_ready),
-        .b           (lut_result      ),
-        .b_valid     (lut_result_valid),
-        .b_ready     (lut_result_ready),
-        .result      (dot_result      ),
-        .result_valid(dot_result_valid),
-        .result_ready(dot_result_ready),
-        .error       (dot_error       )
-    );
-
-
-    localparam IDLE=0, PASS=1, IGNORE=2;
-    reg [1:0] state;
-
-    always @ (posedge clk) begin
-        if (rst) begin
-            state <= IDLE;
-            layer_buffer <= 0;
-            layer_set <= 0;
-        end
-        else begin
-            case (state) 
-                IDLE: begin
-                    state        <= (layer_set) ? (layer_buffer > 0) ? PASS : IGNORE : IDLE;
-                    layer_buffer <= (!layer_set && layer_valid) ? layer : layer_buffer;
-                    layer_set    <= (!layer_set && layer_valid) ? 1     : layer_set;
-                end
-                PASS: begin
-                    state        <= (dot_result_valid && delta_output_ready) ? IDLE : PASS;
-                    layer_buffer <= (dot_result_valid && delta_output_ready) ? 0    : layer_buffer;
-                    layer_set    <= (dot_result_valid && delta_output_ready) ? 0    : layer_set;
-                end
-                IGNORE: begin
-                    state        <= (dot_result_valid) ? IDLE : IGNORE;
-                    layer_buffer <= (dot_result_valid) ? 0    : layer_buffer;
-                    layer_set    <= (dot_result_valid) ? 0    : layer_set;
-                end
-            endcase
-        end
-    end
+        .clk         (clk               ),
+        .rst         (rst               ),
+        .a           (mvm_result        ),
+        .a_valid     (mvm_result_valid  ),
+        .a_ready     (mvm_result_ready  ),
+        .b           (lut_result        ),
+        .b_valid     (lut_result_valid  ),
+        .b_ready     (lut_result_ready  ),
+        .result      (delta_output      ),
+        .result_valid(delta_output_valid),
+        .result_ready(delta_output_ready),
+        .error       (dot_error         )
+    );                
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////
     // Outputs
     //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    assign layer_ready        = !layer_set;
     assign error              = mvm_error | dot_error;
-    assign delta_output       = dot_result;
-    assign delta_output_valid = (state == PASS) ? dot_result_valid : 0;
-    assign dot_result_ready   = (state == PASS) ? delta_output_ready : 1;
     
     ////////////////////////////////////////////////////////////////////////////////////////////////////  
     // Testing
     ////////////////////////////////////////////////////////////////////////////////////////////////////  
     
-    wire [ACTIVATION_WIDTH-1:0] activation_mem [MATRIX_HEIGHT-1:0];
-    wire [DELTA_CELL_WIDTH-1:0] mvm_mem        [MATRIX_HEIGHT-1:0];
-    wire [DELTA_CELL_WIDTH-1:0] dot_mem        [MATRIX_HEIGHT-1:0];
-    //wire [DELTA_CELL_WIDTH-1:0] delta_mem      [MATRIX_WIDTH-1:0];
+    //wire [ACTIVATION_WIDTH-1:0] activation_mem [MATRIX_HEIGHT-1:0];
+    //wire [DELTA_CELL_WIDTH-1:0] mvm_mem        [MATRIX_HEIGHT-1:0];
+    //wire [DELTA_CELL_WIDTH-1:0] dot_mem        [MATRIX_HEIGHT-1:0];
+    ////wire [DELTA_CELL_WIDTH-1:0] delta_mem      [MATRIX_WIDTH-1:0];
 
-    genvar i;
-    generate
-    for (i=0; i<MATRIX_HEIGHT; i=i+1) begin: MEM
-        assign activation_mem[i] = lut_result  [i*ACTIVATION_WIDTH+:ACTIVATION_WIDTH];
-        assign mvm_mem[i]        = mvm_result  [i*DELTA_CELL_WIDTH+:DELTA_CELL_WIDTH];
-        assign dot_mem[i]        = delta_output[i*DELTA_CELL_WIDTH+:DELTA_CELL_WIDTH];
-    end
-    endgenerate
+    //genvar i;
+    //generate
+    //for (i=0; i<MATRIX_HEIGHT; i=i+1) begin: MEM
+        //assign activation_mem[i] = lut_result  [i*ACTIVATION_WIDTH+:ACTIVATION_WIDTH];
+        //assign mvm_mem[i]        = mvm_result  [i*DELTA_CELL_WIDTH+:DELTA_CELL_WIDTH];
+        //assign dot_mem[i]        = delta_output[i*DELTA_CELL_WIDTH+:DELTA_CELL_WIDTH];
+    //end
+    //endgenerate
     
 endmodule
