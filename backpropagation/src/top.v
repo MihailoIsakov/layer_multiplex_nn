094905ae9d0249ad3212c64f1e80fd18f9f4d21a
Mihailo Isakov <mihailo@protonmail.com>
2017-07-05 20:25:37 -0400

    updated top to work with the new stack with the reset signal, removed some layer splitter signals

--- backpropagation/src/top.v
+++ backpropagation/src/top.v
@@ -22,7 +22,7 @@ module top #(
     // Wires & regs
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    wire [NEURON_NUM*ACTIVATION_WIDTH-1:0] network_inputs, network_inputs_1, network_inputs_2, input_zero;
+    wire [NEURON_NUM*ACTIVATION_WIDTH-1:0] network_inputs, network_inputs_1, network_inputs_2;
     wire network_inputs_valid, network_inputs_ready;
     wire network_inputs_1_valid, network_inputs_1_ready; 
     wire network_inputs_2_valid, network_inputs_2_ready;
@@ -31,8 +31,6 @@ module top #(
     wire [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0] stack_input, input_zero_extended; 
     wire stack_input_valid;
     wire stack_input_ready;
-    wire input_zero_valid;
-    wire input_zero_ready;
 
     wire [NEURON_NUM*ACTIVATION_WIDTH-1:0] network_targets;
     wire network_targets_valid, network_targets_ready;
@@ -47,9 +45,9 @@ module top #(
     wire fw_layer_number_ready;
 
     // splitter
-    wire [LAYER_ADDR_WIDTH-1:0] layer_fifo_1, layer_fifo_2, layer_fifo_3, layer_fifo_4, layer_fifo_5, layer_fifo_gate, layer_fifo_forward, layer_fifo_backward;
-    wire layer_fifo_1_valid, layer_fifo_2_valid, layer_fifo_3_valid, layer_fifo_4_valid, layer_fifo_5_valid, layer_fifo_gate_valid, layer_fifo_forward_valid, layer_fifo_backward_valid;
-    wire layer_fifo_1_ready, layer_fifo_2_ready, layer_fifo_3_ready, layer_fifo_4_ready, layer_fifo_5_ready, layer_fifo_gate_ready, layer_fifo_forward_ready, layer_fifo_backward_ready;
+    wire [LAYER_ADDR_WIDTH-1:0] layer_fifo_1, layer_fifo_2, layer_fifo_3, layer_fifo_4, layer_fifo_5, layer_fifo_gate;
+    wire layer_fifo_1_valid, layer_fifo_2_valid, layer_fifo_3_valid, layer_fifo_4_valid, layer_fifo_5_valid, layer_fifo_gate_valid;
+    wire layer_fifo_1_ready, layer_fifo_2_ready, layer_fifo_3_ready, layer_fifo_4_ready, layer_fifo_5_ready, layer_fifo_gate_ready;
 
     // outputs from the layer module
     wire [NEURON_NUM*NEURON_OUTPUT_WIDTH-1:0] current_layer_outputs;
@@ -74,19 +72,19 @@ module top #(
     // Datapath 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    fifo_splitter_parametrized #(LAYER_ADDR_WIDTH, 4) 
+    fifo_splitter_parametrized #(LAYER_ADDR_WIDTH, 5) 
     layer_splitter (
         .clk           (clk               ),
         .rst           (rst               ),
         .data_in       (fw_layer_number      ),
         .data_in_valid (fw_layer_number_valid),
         .data_in_ready (fw_layer_number_ready),
-        //.data_out      ({layer_fifo_1, layer_fifo_2, layer_fifo_3, layer_fifo_4, layer_fifo_5}),
-        //.data_out_valid({layer_fifo_1_valid, layer_fifo_2_valid, layer_fifo_3_valid, layer_fifo_4_valid, layer_fifo_5_valid}),
-        //.data_out_ready({layer_fifo_1_ready, layer_fifo_2_ready, layer_fifo_3_ready, layer_fifo_4_ready, layer_fifo_5_ready})
-        .data_out      ({layer_fifo_1, layer_fifo_2, layer_fifo_3, layer_fifo_4}),
-        .data_out_valid({layer_fifo_1_valid, layer_fifo_2_valid, layer_fifo_3_valid, layer_fifo_4_valid}),
-        .data_out_ready({layer_fifo_1_ready, layer_fifo_2_ready, layer_fifo_3_ready, layer_fifo_4_ready})
+        .data_out      ({layer_fifo_1, layer_fifo_2, layer_fifo_3, layer_fifo_4, layer_fifo_5}),
+        .data_out_valid({layer_fifo_1_valid, layer_fifo_2_valid, layer_fifo_3_valid, layer_fifo_4_valid, layer_fifo_5_valid}),
+        .data_out_ready({layer_fifo_1_ready, layer_fifo_2_ready, layer_fifo_3_ready, layer_fifo_4_ready, layer_fifo_5_ready})
+        //.data_out      ({layer_fifo_1, layer_fifo_2, layer_fifo_3, layer_fifo_4}),
+        //.data_out_valid({layer_fifo_1_valid, layer_fifo_2_valid, layer_fifo_3_valid, layer_fifo_4_valid}),
+        //.data_out_ready({layer_fifo_1_ready, layer_fifo_2_ready, layer_fifo_3_ready, layer_fifo_4_ready})
     );
 
     fifo_splitter_parametrized #(LAYER_ADDR_WIDTH, 2)
@@ -139,7 +137,6 @@ module top #(
 
     extend #(NEURON_NUM, ACTIVATION_WIDTH, NEURON_OUTPUT_WIDTH-ACTIVATION_WIDTH)
     extend (
-        //.in(input_zero),
         .in(network_inputs_2),
         .out(input_zero_extended)
     );
@@ -158,21 +155,6 @@ module top #(
         .result      (layer_fifo_gate         ),
         .result_valid(layer_fifo_gate_valid   ),
         .result_ready(layer_fifo_gate_ready   )
-    );
-
-    fifo_splitter2 #(LAYER_ADDR_WIDTH) 
-    layer_splitter_fw_bw (
-        .clk            (clk                      ),
-        .rst            (rst                      ),
-        .data_in        (layer_fifo_gate          ),
-        .data_in_valid  (layer_fifo_gate_valid    ),
-        .data_in_ready  (layer_fifo_gate_ready    ),
-        .data_out1      (layer_fifo_forward       ),
-        .data_out1_valid(layer_fifo_forward_valid ),
-        .data_out1_ready(layer_fifo_forward_ready ),
-        .data_out2      (layer_fifo_backward      ),
-        .data_out2_valid(layer_fifo_backward_valid),
-        .data_out2_ready(layer_fifo_backward_ready)
     );
 
 
@@ -199,9 +181,9 @@ module top #(
         .weights                    (weights                    ),
         .weights_valid              (weights_valid              ),
         .weights_ready              (weights_ready              ),
-        .layer_number               (layer_fifo_forward         ),
-        .layer_number_valid         (layer_fifo_forward_valid   ),
-        .layer_number_ready         (layer_fifo_forward_ready   ),
+        .layer_number               (layer_fifo_5               ),
+        .layer_number_valid         (layer_fifo_5_valid         ),
+        .layer_number_ready         (layer_fifo_5_ready         ),
         .current_layer_outputs      (current_layer_outputs      ),
         .overflow                   (fw_overflow                ),
         .current_layer_outputs_valid(current_layer_outputs_valid),
@@ -217,8 +199,6 @@ module top #(
         .a_valid     (current_layer_outputs_valid),
         .a_ready     (current_layer_outputs_ready),
         .b           (input_zero_extended        ),
-        //.b_valid     (input_zero_valid           ),
-        //.b_ready     (input_zero_ready           ),
         .b_valid     (network_inputs_2_valid     ),
         .b_ready     (network_inputs_2_ready     ),
         .select      (layer_fifo_2 == 0          ),
@@ -237,6 +217,7 @@ module top #(
         .STACK_ADDR_WIDTH(LAYER_MAX          ))
     stack (
         .clk                     (clk                  ),
+        .rst                     (rst                  ),
         .input_data              (stack_input          ),
         .input_data_valid        (stack_input_valid    ),
         .input_data_ready        (stack_input_ready    ),
@@ -268,27 +249,27 @@ module top #(
         .LAYER_MAX          (LAYER_MAX          ),
         .WEIGHT_INIT_FILE   (WEIGHT_INIT_FILE   )
     ) backpropagator (
-        .clk           (clk                      ),
-        .rst           (rst                      ),
-        .layer_bw      (bw_layer_fifo_2          ),
-        .layer_bw_valid(bw_layer_fifo_2_valid    ),
-        .layer_bw_ready(bw_layer_fifo_2_ready    ),
-        .layer_fw      (layer_fifo_backward      ),
-        .layer_fw_valid(layer_fifo_backward_valid),
-        .layer_fw_ready(layer_fifo_backward_ready),
-        .sample        (network_targets          ),
-        .sample_valid  (network_targets_valid    ),
-        .sample_ready  (network_targets_ready    ),
-        .z             (stack_lower              ),
-        .z_valid       (stack_lower_valid        ),
-        .z_ready       (stack_lower_ready        ),
-        .z_prev        (stack_higher             ),
-        .z_prev_valid  (stack_higher_valid       ),
-        .z_prev_ready  (stack_higher_ready       ),
-        .weights       (weights                  ),
-        .weights_valid (weights_valid            ),
-        .weights_ready (weights_ready            ),
-        .error         (bw_overflow              )
+        .clk           (clk                  ),
+        .rst           (rst                  ),
+        .layer_bw      (bw_layer_fifo_2      ),
+        .layer_bw_valid(bw_layer_fifo_2_valid),
+        .layer_bw_ready(bw_layer_fifo_2_ready),
+        .layer_fw      (layer_fifo_gate      ),
+        .layer_fw_valid(layer_fifo_gate_valid),
+        .layer_fw_ready(layer_fifo_gate_ready),
+        .sample        (network_targets      ),
+        .sample_valid  (network_targets_valid),
+        .sample_ready  (network_targets_ready),
+        .z             (stack_higher         ),
+        .z_valid       (stack_higher_valid   ),
+        .z_ready       (stack_higher_ready   ),
+        .z_prev        (stack_lower          ),
+        .z_prev_valid  (stack_lower_valid    ),
+        .z_prev_ready  (stack_lower_ready    ),
+        .weights       (weights              ),
+        .weights_valid (weights_valid        ),
+        .weights_ready (weights_ready        ),
+        .error         (bw_overflow          )
     );
 
     
@@ -306,7 +287,7 @@ module top #(
     assign bottom_layer_write = backpropagator.weight_controller.weights_bram.bram.writeEnable0 &&
                                 backpropagator.weight_controller.weights_bram.bram.writeAddress0 == 0;
 
-    localparam FW=0, BW=1, FW_STEP=2, BW_STEP=3;
+    localparam FW=0, FW_STEP=1, BW=2, BW_STEP=3;
     reg [1:0] state;
 
     reg underflow; // the bw_layer_number underflows and causes issues. FIXME find a better way
